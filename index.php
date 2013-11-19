2165508a21a9d097ca622ad5ce21e83bfd7662e0
ayafuji <ayafuji.aya@gmail.com>
2013-11-20 06:39:37 +0900

    add main visual

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 	</head>
 	<body>
 		<div id="wrap">
-			<div id="header">
 				<div id="logo">
 					<table>
 						<tr>
@@ -23,8 +22,21 @@
 						</tr>
 					</table>	
 				</div><!-- #logo -->
+					<div id="contact">
+						<table>
+							<tr>
+								<td><img src="./img/tell.png"></td>
+								<td><img src="./img/twitter_header.png"></td>
+								<td><img src="./img/like_facebook.png"></td>
+							</tr>
+							<tr>
+								<td colspan="3"><img src="./img/mail.png"></td>
+							</tr>
+						</table>
+					</div>
+			<div id="header">
 				<div id="nav">
-					<table>
+					<table id="nav_table">
 						<tr align="center" style="margin:0 25px 0 25px">
 							<td class="nav_image" style="padding:0 50px;"><img src="./img/about.png"></td>
 							<td class="nav_image" style="padding:0 50px;"><img src="./img/bio.png"></td>
@@ -44,12 +56,28 @@
 				<div class="flexslider">
 					<ul class="slides">
 						<li>
-							<img src="img/aeri/IMG_4417.png"/>
+							<img src="img/aeri/06.JPG"/>
+						</li>
+						<li>
+							<img src="img/aeri/03.JPG"/>
 						</li>
 					</ul>
 				</div>
 			</div><!-- #slide -->
+			<div id="text_information">
+				渋家新メンバー！　池田愛恵里さんです -> Official Web <a href="http://ameblo.jp/ikeda-aeri/">池田愛恵里オフィシャルブログ「ようこそ池田農園へ。」 powered by Ameba</a>
+			</div>
 			<div id="hot_topics">
+				<div id="hot_topics_pic">
+					<img src="./img/search.png"><img id="hot_topics_midashi" src="./img/hot_topics.png">
+				</div><!-- hot_topics_pic -->
+				<div id="hot_topics_text">
+					<ul>
+						<li class="topics"><sep class="topics_date">2013/10/11</sep><img src="./img/arrow.png"><sep class="topics_date">アイコンを作ってみました＝〜〜〜〜〜！！！</sep></li>
+						<li class="topics"><sep class="topics_date">2013/10/11</sep><img src="./img/arrow.png"><sep class="topics_date">アイコンを作ってみました＝〜〜〜〜〜！！！</sep></li>
+						<li class="topics"><sep class="topics_date">2013/10/11</sep><img src="./img/arrow.png"><sep class="topics_date">アイコンを作ってみました＝〜〜〜〜〜！！！</sep></li>
+					</ul>
+				</div><!-- hot_topics_text -->
 			</div><!-- #hot_topics -->
 			<div id="content">
 				<div id="what" class="midashi">
