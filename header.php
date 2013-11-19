85331fe2d92bd91406aae1bc67f2f6ee43a01d65
ayafuji <ayafuji.aya@gmail.com>
2013-11-20 08:08:56 +0900

    add header file

---
+++ header.php
@@ -0,0 +1,40 @@
+				<div id="logo">
+					<table>
+						<tr>
+							<img src="img/logo.png"></br>
+						</tr>
+						<tr>
+							<p id="text_logo">シブハウスオフィシャルホームページ</p>
+						</tr>
+					</table>	
+				</div><!-- #logo -->
+					<div id="contact">
+						<table>
+							<tr>
+								<td><img src="./img/tell.png"></td>
+								<td><img src="./img/twitter_header.png"></td>
+								<td><img src="./img/like_facebook.png"></td>
+							</tr>
+							<tr>
+								<td colspan="3"><img src="./img/mail.png"></td>
+							</tr>
+						</table>
+					</div>
+			<div id="header">
+				<div id="nav">
+					<table id="nav_table">
+						<tr align="center" style="margin:0 25px 0 25px">
+							<td class="nav_image" style="padding:0 50px;"><img src="./img/about.png"></td>
+							<td class="nav_image" style="padding:0 50px;"><img src="./img/bio.png"></td>
+							<td class="nav_image" style="padding:0 50px;"><img src="./img/life.png"></td>
+							<td class="nav_image" style="padding:0 50px;"><img src="./img/member.png"></td>
+						</tr>
+						<tr>
+							<td style="padding:0 45px;">アバウト＞</td>
+							<td style="padding:0 21px;">バイオグラフィー＞</td>
+							<td style="padding:0 50px;">ライフ＞</td>
+							<td style="padding:0 45px;">メンバー＞</td>
+						</tr>
+					</table>
+				</div><!-- #nav -->
+			</div><!-- #header -->
