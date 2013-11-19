996b37d88e9907c6d5f8f4dea3915b6c909d8c14
ayafuji <ayafuji.aya@gmail.com>
2013-11-19 17:14:24 +0900

    edit index.php

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-hello, world
+<!DOCTYPE html>
+<html>
+	<head>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	</head>
+	<body>
+		<div id="wrap">
+			<div id="headar">
+			</div><!-- #header -->
+			<div id="slide">
+			</div><!-- #slide -->
+			<div id="hot_topics">
+			</div><!-- #hot_topics -->
+			<div id="content">
+				<div id="what" class="midashi">
+				</div><!-- #what -->
+				<div id="bio" class="midashi">
+				</div><!-- #bio -->
+				<div id="member" class="midashi">
+				</div><!-- member -->
+				<div id="information" class="midashi">
+				</div><!-- #information -->
+			</div><!-- #content -->
+		</div><!-- #wrap -->
+		<footer>
+		</footer>
+	</body>
+</html>
