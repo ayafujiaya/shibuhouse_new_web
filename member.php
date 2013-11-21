e7e2fd39ce7e89885c534e1785251c2f937e2cda
ayafuji <ayafuji.aya@gmail.com>
2013-11-21 18:52:23 +0900

    add caption

--- member.php
+++ member.php
@@ -26,7 +26,16 @@
 				<div id="inner_member_profile">
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/01.gif" >
-						<a  class="profile_name" title="AERI IKEDA" rel="lightbox[imagegroup]" data-lightbox="roadtrip" href="./img/aeri/01.gif" style="">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" rel="lightbox[imagegroup]" data-lightbox="roadtrip" href="./img/aeri/01.gif" style="">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -34,7 +43,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/02.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/02.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/02.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -42,7 +60,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/03.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/03.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/03.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -50,7 +77,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/04.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/04.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/04.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -58,7 +94,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/05.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/05.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/05.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -66,7 +111,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/06.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/06.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/06.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -74,7 +128,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/07.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/07.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/07.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -82,7 +145,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/09.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/09.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/09.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -90,7 +162,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/10.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/10.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/10.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -98,7 +179,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/11.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/11.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/11.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -106,7 +196,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/12.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/12.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/12.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -114,7 +213,16 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/13.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/13.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/13.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
@@ -122,12 +230,41 @@
 					</div>
 					<div class="profile">
 						<img class="small_image" src="./img/aeri/13.gif">
-						<a  class="profile_name" title="AERI IKEDA" data-lightbox="roadtrip" href="./img/aeri/13.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/aeri/13.gif" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">AERI IKEDA</p>
 						</div>
 						</a>
 					</div>
+					<div class="profile">
+						<img class="small_image" src="./img/member_big_image.png">
+						<a  class="profile_name" title="AERI IKEDA 
+						<div class='explain'>
+							<h1>Yuki Takada</h1>
+							<div class='explain_text'>
+								集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
+Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+							</div>
+						</div>
+						" data-lightbox="roadtrip" href="./img/member_big_image.png" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						<div>
+							<p class="full_name">AERI IKEDA</p>
+						</div>
+						</a>
+					</div>
+
+
+					
 				</div>
 			</div>
 		</div><!-- #wrap -->
