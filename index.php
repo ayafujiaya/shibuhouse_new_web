a59bb3051d7f49c21fedfd33900768dd093e45fa
ayafuji <ayafuji.aya@gmail.com>
2013-11-22 15:39:01 +0900

    iphone design

--- index.php
+++ index.php
@@ -6,11 +6,18 @@
 		<script type="text/javascript" src="//typesquare.com/accessor/script/typesquare.js?E2mGUC7TkZ8%3D" charset="utf-8"></script>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-		<link href='./css/reset.css' rel='stylesheet' type='text/css'>
-		<link href='./css/global.css' rel='stylesheet' type='text/css'>
+		<link href='./css/reset.css' rel='stylesheet' type='text/css'>	
 		<!-- スライドショー -->
 		<link href='./css/flexslider.css' rel='stylesheet' type='text/css'>
+		<link href="css/lightbox.css" rel="stylesheet" />
 		<script type="text/javascript" src="js/modernizr.js"></script>
+		<script type="text/javascript">
+		if (navigator.userAgent.indexOf('iPhone') != -1) {
+			document.write("<link href='./css/iphone.css' rel='stylesheet' type='text/css'>");
+		} else {
+			document.write("<link href='./css/global.css' rel='stylesheet' type='text/css'>");
+		}
+		</script>
 	</head>
 	<body>
 		<div id="wrap">
@@ -18,20 +25,8 @@
 						<div id="slide">
 				<div class="flexslider">
 					<ul class="slides">
-						<li><img src="./img/shibuhouse_01.jpg"></li>
-						<li><img src="./img/shibuhouse_02.jpg"></li>
-						<li><img src="./img/shibuhouse_03.jpg"></li>
-						<!--
-						<li>
-							<img src="./img/aeri/aeri603-1 copy.jpg">
-						</li>
-						<li>
-							<img src="./img/aeri/aeri527-1 copy.jpg"/>
-						</li>
-						<li>
-							<img src="./img/aeri/aeri610-2 copy.jpg"/>
-						</li>
-					-->
+						<li><img src="./img/aeri/main01.gif"></li>
+						<li><img src="./img/aeri/main02.gif"></li>
 					</ul>
 				</div>
 			</div><!-- #slide -->
@@ -55,7 +50,7 @@
 					<div style="padding-top:28px; padding-bottom:26px;" class="inner_midashi">
 						<img  style="margin-bottom:47px" src="./img/whats_shibuhouse.png">
 					<div class="midashi_text">
-						集団的知性（しゅうだんてきちせい、英語：Collective Intelligence、CI）は、多くの個人の協力と競争の中から、その集団自体に知能、精神が存在するかのように見える知性である。Peter Russell（1983年）、Tom Atlee（1993年）、Howard Bloom（1995年）、Francis Heylighen（1995年）、ダグラス・エンゲルバート、Cliff Joslyn、Ron Dembo、Gottfried Mayer-Kress（2003年）らが理論を構築した。
+						<?php require 'text/what.php' ?>
 					</div>
 					<img style="margin:0 auto; display: block;" src="./img/hatena_midashi.png">
 					<img style="margin:0 auto; display: block;" src="./img/about_midahsi.png">
@@ -66,8 +61,7 @@
 					<div style="padding-top:28px; padding-bottom:26px;" class="inner_midashi">
 						<img style="margin-bottom:47px" src="./img/bio_midashi.png">
 						<div class="midashi_text">
-						集団的知性は、細菌、動物、人間、コンピュータなど様々な集団の、意思決定の過程で発生する。集団的知性の研究は、社会学、計算機科学、集団行動の研究[1]などに属する。
-Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから一歩進み、人間の集団的知性に研究の焦点をあてている。Atlee は集団的知性を「集団思考（集団浅慮）や個人の認知バイアスに打ち勝って集団が協調し、より高い知的能力を発揮するため」のものと主張している。
+						<?php require './text/bio.php' ?>
 						</div>					
 						<img style="margin:0 auto; display: block;" src="./img/bio_images.png">
 					</div><!-- .inner_midashi -->
@@ -77,7 +71,7 @@ Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから�
 					<div style="padding-top:28px; padding-bottom:26px;" class="inner_midashi">
 						<img style="margin-bottom:47px" src="./img/member_midashi.png">
 						<div class="midashi_text">
-						集団的知性研究のパイオニアである George Por は、集団的知性現象を「協調と革新を通してより高次の複雑な思考、問題解決、統合を勝ち取りえる、人類コミュニティの能力」と定義している[2]。Tom Atlee と George Por は「集団的知性は、関心をひとつに集中し、適切な行動を選択するための基準を形成する能力がある」と述べている。彼らのアプローチは Scientific Community Metaphor を起源としている。
+						<?php require './text/member.php' ?>
 						</div>					
 						<img style="margin:0 auto 30px auto; display: block;" src="./img/midashi_smile.png">
 						<img style="margin:0 auto; display: block;" src="./img/midashi_member.png">
@@ -88,7 +82,7 @@ Tom Atlee らは、Howard Bloom が「グループIQ」と呼んだものから�
 					<div style="padding-top:28px; padding-bottom:26px;" class="inner_midashi">
 						<img style="margin-bottom:47px" src="./img/information.png">
 						<div class="midashi_text">
-						集合知には、collective intelligence, collective knowledge, wisdom of crowdsなどの異なる英語が対応する。経営学の一分野である知識管理論からのアプローチには、洞口治夫(Horaguchi Haruo)『集合知の経営－日本企業の知識管理戦略－』がある。
+						<?php require './text/information.php' ?>	
 						</div>					
 						<img style="margin:0 auto 30px auto; display: block;" src="./img/midashi_mail.png">
 						<img style="margin:0 auto 16px auto; display: block;" src="./img/call_number.png">
