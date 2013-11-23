406cb4fed3159dd47954486a95bad71a3492c07a
yukiishida <amusing365@gmail.com>
2013-11-23 21:23:56 +0900

    add table fix

--- text/bio/art.php
+++ text/bio/art.php
@@ -28,32 +28,35 @@
 <td>「via art 2010」/シンワアートミュージアム（グループ展）</td>
 </tr>
 <tr>
-<th>2011.</th>
-<td>データセル3-1</td>
+<th>2011.01</th>
+<td>「わくわく SHIBUYA」/TOKYO WONDER SITE（グループ展）</td>
 </tr>
 <tr>
-<th>見出しセル3</th>
-<td>データセル3-1</td>
+<th>2011.10</th>
+<td>「ArtGigTokyo2 - 怖いよ、ママー! -」/旧玉井病院（パフォーマンス出演）</td>
 </tr>
 <tr>
-<th>見出しセル3</th>
-<td>データセル3-1</td>
+<th>2011.11</th>
+<td>「ULTRA004」/スパイラルガーデン（グループ展）</td>
 </tr>
 <tr>
-<th>見出しセル3</th>
-<td>データセル3-1</td>
+<th>2012.05</th>
+<td>「Village Project HOUSE 100」/The Conteiner（個展）</td>
 </tr>
 <tr>
-<th>見出しセル3</th>
-<td>データセル3-1</td>
+<th>2012.11</th>
+<td>「TRANS ARTS TOKYO」/旧東京電機大学校舎11号館（グループ展）</td>
 </tr>
 <tr>
-<th>見出しセル3</th>
-<td>データセル3-1</td>
+<th>2013.03</th>
+<td>「アートフェア東京」/東京国際フォーラム（グループ展）</td>
 </tr>
 <tr>
-<th>見出しセル3</th>
-<td>データセル3-1</td>
+<th>2013.04</th>
+<td>「渋家「イケる気がする展」- 第一回渋家アート選手権大会 -」/HIGURE 17-15 cas（個展）</td>
 </tr>
-
+<tr>
+<th>2013.07</th>
+<td>「Previous Exibitions」/Ultra Super New（グループ展）</td>
+</tr>
 </table>
