9d5b8f111c5670c8ac089f979c9a55753a71a83e
kentacobayashi <saru.gorou@gmail.com>
2013-11-27 02:39:25 +0900

    Update 100.php

--- text/bio_explain/100.php
+++ text/bio_explain/100.php
@@ -5,5 +5,61 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-  ハウス100本文変更
+Village Project HOUSE 100<br />
+2012年5月21日～7月30日<br />
+The Container<br />
+http://shibuhouse.com/1902<br />
+<br />
+Artist statement &quot;OUR HOUSE&quot; time specific village project&quot;HOUSE100&quot;<br />
+<br />
+<br />
+渋家は2008年に渋谷で始まりました。<br />
+新しい形のシェア、新しい形のパブリックを思考し運営することで、<br />
+アートの可能性を追求しようとする、コミュニティを内包したアートプロジェクトです。<br />
+<br />
+私達は20人を超えるメンバーで家をシェアしています。<br />
+それは既存の家族というコミュニティとは違う形の連帯を生んでいます。<br />
+<br />
+私達は一人一人が目標を持っています。<br />
+その目標はそれぞれ全く別の方向を向いています。<br />
+しかし一つのコミュニティである以上、私達はそれぞれの目標を尊重します。<br />
+<br />
+今回の個展は、そんな私達が「どのような村に住みたいか」という提案です。<br />
+私達は今まで何度も引っ越しを繰り返してきました。つまりハードウエアにはこだわりません。<br />
+私達が重要だと思うのは「どのような人たちと一緒に住みたいか」という選択です。<br />
+<br />
+「世界がもし100人の村だったら」という文章を知っていますか？<br />
+あるいは「世界で最も影響力のある100人のリスト」という企画を知っていますか？<br />
+今回の個展で私達は100人の村人になって欲しい、影響を受けたい人たちを選び、インタビューします。<br />
+そしてその後に私達の村の中心である渋家で一緒に食事をします。<br />
+新しい村を作るきっかけになるという期待を込めて。<br />
+<br />
+それは歴史上初めての、あるいは古くから幾度となく存在していた「時間軸上に出現する村」となるでしょう。 <br />
+魅力的な人々から影響を受け、時には影響を与えることが、ネットワークとなり、村人を規定するのです。<br />
+<br />
+だから私達は、記録としてではなく記憶として、今回の個展を遺そうと思います。<br />
+USTream,twitter,そして写真は、そんな私達にピッタリのメディアです。<br />
+<br />
+インタビューをUSTreamで配信し、twitterで実況し、写真を撮影し、ギャラリーというアルバムに飾る。<br />
+それが私達の今回の展示の全てです。<br />
+<br />
+これから私達は村を創ります。<br />
+それは私達が永遠に関わることのできる村となるはずです。<br />
+どんなことがあっても友愛と共にある村となることを私達は願っています。<br />
+そして希望が失われない場所となることを祈っています。<br />
+<br />
+最後にキュレーターshai ohayon、ギャラリーThe Containerを始め、関わってくださる全ての方に、そして興味を持ってくださる全ての方に、感謝します。<br />
+よろしくお願いいたします。<br />
+<br />
+2012.5.21 渋家 / shibuhouse 一同<br />
+<br />
+<br />
+<br />
+<br />
+Shibuhouse started in 2008 in Shibuya.<br />
+It is an art project and also a community which try to pursuit the possibility of art by organizing a new form sharing and new form of public.<br />
+<br />
+We are sharing a house with over 20 members. <br />
+This community means something different from just a family. <br />
+Each one of us have different aims.
 </div>
