c742bea3e2ac0681802349cb0249522e2ab6314a
yukiishida <amusing365@gmail.com>
2013-11-26 18:08:26 +0900

    Create ult.php

---
+++ text/bio_explain/ult.php
@@ -0,0 +1,9 @@
+<div id="ex_image">
+	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+</div>
+<div id="ex_text">
+  ULTRA004　本文
+</div>
