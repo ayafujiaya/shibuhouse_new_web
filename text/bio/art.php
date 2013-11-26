edf3b91628c0ab21a8080a25d47df7ad49dab69f
yukiishida <amusing365@gmail.com>
2013-11-26 17:13:14 +0900

    add onclick

--- text/bio/art.php
+++ text/bio/art.php
@@ -1,62 +1,62 @@
 <table>
 <tr>
 <th>2008.10</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/01.php");'>「お好きなものをお選びください展」/デザインフェスタギャラリー（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/osu.php");'>「お好きなものをお選びください展」/デザインフェスタギャラリー（グループ展）</td>
 </tr>
 <tr>
 <th>2009.02</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/02.php");'>「リンカイ展」/多摩美術大学上野毛キャンパス（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/rin.php");'>「リンカイ展」/多摩美術大学上野毛キャンパス（個展）</td>
 </tr>
 <tr>
 <th>2010.03</th>
-<td>「フラグメントシアター/断片化の解消」/3331 Arts Chiyoda（パフォーマンス出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hur.php");'>「フラグメントシアター/断片化の解消」/3331 Arts Chiyoda（パフォーマンス出演）</td>
 </tr>
 <tr>
 <th>2010.03</th>
-<td>「BAR☆Time Line」/3331 Arts Chiyoda（パフォーマンス出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/bar.php");'>「BAR☆Time Line」/3331 Arts Chiyoda（パフォーマンス出演）</td>
 </tr>
 <tr>
 <th>2010.11</th>
-<td>「渋家トリエンナーレ」/3rd house（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tri.php");'>「渋家トリエンナーレ」/3rd house（個展）</td>
 </tr>
 <tr>
 <th>2010.12</th>
-<td>「VIVA☆校」/棚ガレリ（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/viv.php");'>「VIVA☆校」/棚ガレリ（個展）</td>
 </tr>
 <tr>
 <th>2010.12</th>
-<td>「via art 2010」/シンワアートミュージアム（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/via.php");'>「via art 2010」/シンワアートミュージアム（グループ展）</td>
 </tr>
 <tr>
 <th>2011.01</th>
-<td>「わくわく SHIBUYA」/TOKYO WONDER SITE（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/wak.php");'>「わくわく SHIBUYA」/TOKYO WONDER SITE（グループ展）</td>
 </tr>
 <tr>
 <th>2011.10</th>
-<td>「ArtGigTokyo2 - 怖いよ、ママー! -」/旧玉井病院（パフォーマンス出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/gig.php");'>「ArtGigTokyo2 - 怖いよ、ママー! -」/旧玉井病院（パフォーマンス出演）</td>
 </tr>
 <tr>
 <th>2011.11</th>
-<td>「ULTRA004」/スパイラルガーデン（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ult.php");'>「ULTRA004」/スパイラルガーデン（グループ展）</td>
 </tr>
 <tr>
 <th>2012.05</th>
-<td>「Village Project HOUSE 100」/The Conteiner（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/100.php");'>「Village Project HOUSE 100」/The Conteiner（個展）</td>
 </tr>
 <tr>
 <th>2012.11</th>
-<td>「TRANS ARTS TOKYO」/旧東京電機大学校舎11号館（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tra.php");'>「TRANS ARTS TOKYO」/旧東京電機大学校舎11号館（グループ展）</td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td>「アートフェア東京」/東京国際フォーラム（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/aft.php");'>「アートフェア東京」/東京国際フォーラム（グループ展）</td>
 </tr>
 <tr>
 <th>2013.04</th>
-<td>「渋家「イケる気がする展」- 第一回渋家アート選手権大会 -」/HIGURE 17-15 cas（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ike.php");'>「渋家「イケる気がする展」- 第一回渋家アート選手権大会 -」/HIGURE 17-15 cas（個展）</td>
 </tr>
 <tr>
 <th>2013.07</th>
-<td>「Previous Exibitions」/Ultra Super New（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/usn.php");'>「Previous Exibitions」/Ultra Super New（グループ展）</td>
 </tr>
 </table>
