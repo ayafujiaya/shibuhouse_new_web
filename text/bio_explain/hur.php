485f1635d726af0aad731faf3ed7bf8bf25fe545
yukiishida <amusing365@gmail.com>
2013-11-26 18:03:08 +0900

    Update

--- text/bio_explain/hur.php
+++ text/bio_explain/hur.php
@@ -1,1 +1,9 @@
-フラグメントシアター
+<div id="ex_image">
+	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+</div>
+<div id="ex_text">
+  フラグメントシアター本文
+</div>
