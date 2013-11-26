7797b8b4d5c04be913b4b792aa6a08ff5dd71fe7
kentacobayashi <saru.gorou@gmail.com>
2013-11-27 02:47:15 +0900

    Update bar.php

--- text/bio_explain/bar.php
+++ text/bio_explain/bar.php
@@ -5,5 +5,11 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-  BAR TIME LINE 本文
+なないろチャンネル企画展示参加<br />
+3331 arts Chiyoda<br />
+３月１４日?3月２８日<br />
+<br />
+http://www.youtube.com/watch?v=injpp4R2_QA<br />
+<br />
+http://www.youtube.com/watch?v=LPv0XAWTYcA
 </div>
