a0a353166747489592c678f7821e69ce698e96cc
yukiishida <amusing365@gmail.com>
2013-11-26 17:21:21 +0900

    add onclick

--- text/bio/media.php
+++ text/bio/media.php
@@ -1,74 +1,74 @@
 <table>
 <tr>
 <th>2011.03</th>
-<td>『朝日ジャーナル』(2011.3.19号)/「特集  日本破壊計画 – シェアハウスの人々 -」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/asahi_j.php");'>『朝日ジャーナル』(2011.3.19号)/「特集  日本破壊計画 – シェアハウスの人々 -」</td>
 </tr>
 <tr>
 <th>2011.05</th>
-<td> Tokyo Weekender/「Uncovering a Tokyo discovery」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/twe.php");'> Tokyo Weekender/「Uncovering a Tokyo discovery」</td>
 </tr>
 <tr>
 <th>2011.08</th>
-<td>ガジェット通信/「関係者に聞くシェアハウス”渋家”の過去・現在・未来」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/gaget_1.php");'>ガジェット通信/「関係者に聞くシェアハウス”渋家”の過去・現在・未来」</td>
 </tr>
 <tr>
 <th>2011.11</th>
-<td>『SPA!』(2011.11.1号)/「渋谷で家賃 3 万円の快適シェアハウス」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/spa_1.php");'>『SPA!』(2011.11.1号)/「渋谷で家賃 3 万円の快適シェアハウス」</td>
 </tr>
 <tr>
 <th>2011.11</th>
-<td>『HK 2』/「特集 さあ、〈いっしょに〉暮そう」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hk.php");'>『HK 2』/「特集 さあ、〈いっしょに〉暮そう」</td>
 </tr>
 <tr>
 <th>2011.12</th>
-<td>『DOMMUNE オフィシャルガイドブック 2』[DOMMUNE/河出書房新社]/「特集  2012 年の日本を発電させるカルチャーエネルギーベスト100!!!!!!!! - 渋家というプロジェクトについて -」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/dommune.php");'>『DOMMUNE オフィシャルガイドブック 2』[DOMMUNE/河出書房新社]/「特集  2012 年の日本を発電させるカルチャーエネルギーベスト100!!!!!!!! - 渋家というプロジェクトについて -」</td>
 </tr>
 <tr>
 <th>2012.08</th>
-<td>Art it asia/「渋家」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/aia.php");'>Art it asia/「渋家」</td>
 </tr>
 <tr>
 <th>2012.05</th>
-<td>『東京アートガイド』[美術手帖編集部/美術出版社]</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/artguide.php");'>『東京アートガイド』[美術手帖編集部/美術出版社]</td>
 </tr>
 <tr>
 <th>2012.06</th>
-<td>『地域社会圏主義』[山本理顕/LIXIL出版] </td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tss.php");'>『地域社会圏主義』[山本理顕/LIXIL出版] </td>
 </tr>
 <tr>
 <th>2012.06</th>
-<td>「#エンダン W」(NOTTV)/「渋家」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/endan.php");'>「#エンダン W」(NOTTV)/「渋家」</td>
 </tr>
 <tr>
 <th>2012.07</th>
-<td>「Japan, Fall of the Rising Sun」(BBC Three) </td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/bbc.php");'>「Japan, Fall of the Rising Sun」(BBC Three) </td>
 </tr>
 <tr>
 <th>2012.09</th>
-<td>NETOKARU/「渋谷にあるオルタナティブスペース”渋家”新代表に菅井早苗が就任」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/netokaru_1.php");'>NETOKARU/「渋谷にあるオルタナティブスペース”渋家”新代表に菅井早苗が就任」</td>
 </tr>
 <tr>
 <th>2013.01</th>
-<td>『朝日新聞』(2013.1.1号)/「ひとつ屋根の下、飛躍もシェア」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/asahi_2013.php");'>『朝日新聞』(2013.1.1号)/「ひとつ屋根の下、飛躍もシェア」</td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td>「ニュースセブン」(NHK)/「アートフェア東京」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/nhk_news.php");'>「ニュースセブン」(NHK)/「アートフェア東京」</td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td>2.5D/「「渋家」が新メンバー募集、新料金は 2.5 万」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/25d_news.php");'>2.5D/「「渋家」が新メンバー募集、新料金は 2.5 万」</td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td>Art it asia/「売り渋家」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/art_it_asia.php");'>Art it asia/「売り渋家」</td>
 </tr>
 <tr>
 <th>2013.06</th>
-<td>「もはや神ダネ!」(フジテレビ)/「アーティストの卵が集まるシェアハウスで神ダネ探し」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/kamidane.php");'>「もはや神ダネ!」(フジテレビ)/「アーティストの卵が集まるシェアハウスで神ダネ探し」</td>
 </tr>
 <tr>
 <th>2013.10</th>
-<td>『六本木クロッシング2013展 アウト・オブ・ダウト 来たるべき風景のために』[森美術館/平凡社] /「ディスカーシブ・プラットホーム」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ropponngi_2013.php");'>『六本木クロッシング2013展 アウト・オブ・ダウト 来たるべき風景のために』[森美術館/平凡社] /「ディスカーシブ・プラットホーム」</td>
 </tr>
 </table>
