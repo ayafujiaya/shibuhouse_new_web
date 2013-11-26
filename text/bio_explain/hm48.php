7891a21ec69508c178d9396937617aca7b33ea62
kentacobayashi <saru.gorou@gmail.com>
2013-11-27 03:09:12 +0900

    Update hm48.php

--- text/bio_explain/hm48.php
+++ text/bio_explain/hm48.php
@@ -5,5 +5,52 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-	H&M 48 本文
+2012年 8月 3日 (金曜日)<br />
+21:00@chefoba<br />
+21:40@Dan_chooooooo<br />
+22:20 Silvanian Families<br />
+23:00&nbsp; @yako_flapper3<br />
+2012年 8月 4日 (土曜日)<br />
+00:00@syun_chang<br />
+02:00&nbsp; @junkMa<br />
+03:00&nbsp; @tomad<br />
+04:00&nbsp; mirrorball inferno ( @tamio @e2boye4boy )<br />
+05:00&nbsp; 建設中 (@nnnnnnnnnnn @Geak_Boy @hugabooks)<br />
+08:00&nbsp; DJ換気<br />
+10:00&nbsp; @moriura (DJMIX)<br />
+12:00&nbsp; DEMP@HOLIC<br />
+16:00&nbsp; DJ換気<br />
+17:00&nbsp; @GoddoSukoupion<br />
+18:00&nbsp; ヤバい中村おじトマド ( @kusokuniman @tomad @syuto_nakamura )<br />
+19:00&nbsp; @inu<br />
+20:00&nbsp; @yosshibox<br />
+21:00&nbsp; @soulflower_no1 / @satononono<br />
+22:30&nbsp; @Licaxxx<br />
+23:15&nbsp; Zwei Raketen ( @yoshiki_zr @IVOZ_ZR )<br />
+<br />
+2012年 8月 5日 (日曜日)<br />
+00:00&nbsp; MIXXXTURE(@futatsuki @skiemo1651 @deepriver598)+@seimei1992<br />
+03:00&nbsp; @sskhybrid<br />
+04:00&nbsp; NO DISCO RECORDS<br />
+05:00&nbsp; @Not1nservice<br />
+06:00&nbsp; @Yanmar21<br />
+07:00&nbsp;DJ 換気<br />
+08:00&nbsp; @ishikur0 (UST/ナハウス)<br />
+09:00&nbsp; @hotori<br />
+10:00&nbsp; @thommole<br />
+11:00&nbsp; DJ クリとリス @satoketa<br />
+11:45&nbsp; @hnnhn<br />
+12:30&nbsp; @kusokuniman<br />
+13:15 もかもか<br />
+14:00&nbsp; コバルト爆弾αΩ<br />
+15:00&nbsp; @Miiixn<br />
+16:00&nbsp; @akinyan_75<br />
+17:00&nbsp; @kalapattar<br />
+18:00&nbsp; DJ USYN<br />
+19:00&nbsp; @rnts<br />
+20:00&nbsp; DJ WILDPARTY<br />
+<br />
+<br />
+<br />
+http://hm48.cs8.biz/
 </div>
