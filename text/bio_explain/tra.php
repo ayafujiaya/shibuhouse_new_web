69f1a6b5a30886d4d60ed1b8ca6389b277d554b7
yukiishida <amusing365@gmail.com>
2013-11-26 18:09:31 +0900

    Create tra.php

---
+++ text/bio_explain/tra.php
@@ -0,0 +1,9 @@
+<div id="ex_image">
+	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+</div>
+<div id="ex_text">
+  トランスアート東京(旧電機大学)　本文
+</div>
