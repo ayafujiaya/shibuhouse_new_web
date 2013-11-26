22b11714a38dba885c3edadd598d10c5e00a8b68
yukiishida <amusing365@gmail.com>
2013-11-26 17:16:51 +0900

    add onclick

--- text/bio/event.php
+++ text/bio/event.php
@@ -1,54 +1,54 @@
 <table>
 <tr>
 <th>2010.06</th>
-<td>「渋家パーティー」/amate-raxi（主催）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/amate.php");'>「渋家パーティー」/amate-raxi（主催）</td>
 </tr>
 <tr>
 <th>2011.02</th>
-<td>「house nation」/3rd house（主催）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/nation.php");'>「house nation」/3rd house（主催）</td>
 </tr>
 <tr>
 <th>2011.04</th>
-<td>「OUT OF HOUSE -シャッキシャキのキャベツを添えて-」/amate-raxi（主催）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/outof.php");'>「OUT OF HOUSE -シャッキシャキのキャベツを添えて-」/amate-raxi（主催）</td>
 </tr>
 <tr>
 <th>2012.03</th>
-<td>「Door code.」/eleven（出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/door.php");'>「Door code.」/eleven（出演）</td>
 </tr>
 <tr>
 <th>2012.05</th>
-<td>「Shibuhouse live party」/絵空箱（主催）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/esora.php");'>「Shibuhouse live party」/絵空箱（主催）</td>
 </tr>
 <tr>
 <th>2012.05</th>
-<td>「REPUBLIC VOL.9 - 映像作家100人 -」/WOMB（出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_9.php");'>「REPUBLIC VOL.9 - 映像作家100人 -」/WOMB（出演）</td>
 </tr>
 <tr>
 <th>2012.06</th>
-<td>「歌舞伎町マルチネフューチャーパーク」/ニュージャパン（出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/future.php");'>「歌舞伎町マルチネフューチャーパーク」/ニュージャパン（出演）</td>
 </tr>
 <tr>
 <th>2012.08</th>
-<td>「H&M48 - home party for 48 hours -」/3rd house（主催）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hm48.php");'>「H&M48 - home party for 48 hours -」/3rd house（主催）</td>
 </tr>
 <tr>
 <th>2012.11</th>
-<td>「インターネットヤミ市」/旧東京電機大学校舎11号館 （出店）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/yami_1.php");'>「インターネットヤミ市」/旧東京電機大学校舎11号館 （出店）</td>
 </tr>
 <tr>
 <th>2012.12</th>
-<td>「REPUBLIC VOL.10 - THE FINAL -」/WOMB（出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_final.php");'>「REPUBLIC VOL.10 - THE FINAL -」/WOMB（出演）</td>
 </tr>
 <tr>
 <th>2013.06</th>
-<td> 「DUM-DUM PARTY 2013」/Shibuya O-WEST（出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/dumdum.php");'> 「DUM-DUM PARTY 2013」/Shibuya O-WEST（出演）</td>
 </tr>
 <tr>
 <th>2013.06</th>
-<td>「インターネットヤミ市2」/カイブツ社（出店）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/yami_2.php");'>「インターネットヤミ市2」/カイブツ社（出店）</td>
 </tr>
 <tr>
 <th>2013.06</th>
-<td>「VIVIVI」/Club Asia（出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/vivivi.php");'>「VIVIVI」/Club Asia（出演）</td>
 </tr>
 </table>
