d7681d39e6aa547ed83357e563890dcbc66925b6
yukiishida <amusing365@gmail.com>
2013-11-28 01:31:41 +0900

    Update 5th.php

--- text/bio_explain/5th.php
+++ text/bio_explain/5th.php
@@ -1,8 +1,8 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/event_sum/5th/5th_001.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/5th/5th_002.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/5th/5th_003.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/5th/5th_004.jpg">
 </div>
 <div id="ex_text">
 「渋家5周年PARTY」<br />
