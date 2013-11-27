a7bed12ecbd945990ecc463533198949579f2421
yukiishida <amusing365@gmail.com>
2013-11-28 00:52:11 +0900

    Create tennen.php

---
+++ text/bio_explain/tennen.php
@@ -0,0 +1,13 @@
+<div id="ex_image">
+	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+</div>
+<div id="ex_text">
+『天然生活』<br />
+記事名：「物々交換を開催してみました」<br />
+記事号：2012.8号<br />
+<br />
+雑誌『天然生活』と「物々交換所」が共同開催した、洋服、器、バッグなどを無料で交換し合う「物々交換会」の会場として渋家が使用され、当日の様子がイベントリポートとして誌面に掲載された。
+</div>
