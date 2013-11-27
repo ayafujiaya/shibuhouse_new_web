9b8a3d171816fe9df8f9967f4395e5544cd6a509
yukiishida <amusing365@gmail.com>
2013-11-27 19:53:39 +0900

    Update repub_9.php

--- text/bio_explain/repub_9.php
+++ text/bio_explain/repub_9.php
@@ -5,5 +5,13 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-	リパブリック vol.9　本文
+「REPUBLIC VOL.9 - 映像作家100人 -」<br />
+日程：2012.5 .19<br />
+会場：WOMB<br />
+<br />
+exonemoがMaltine Records主催イベント「なんかもうやけくそでサマーオブラブ」にて野良VJの存在を知ったことをきっかけにプロジェクターを肩に担いでiPhoneで操作をするiPhoneアプリ「VideoBomber」を開発。このアプリを用いて、白い全身タイツを着た渋家メンバーたちに人間スクリーンとして映像を投射するというVJパフォーマンスを行った。天井の高いメインフロアでさまざまな映像がスクリーンに固定される事なく会場中を動き回るこれまでにない圧巻のパフォーマンスだった。<br />
+<br />
+DJ：tomad<br />
+VJ(Video Bomber set)：exonemo / yang02 / YAVAO / 彩藤 / としくに / スケブリ<br />
+人間スクリーン：渋家
 </div>
