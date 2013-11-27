ae838fd972328aac5a45528c7eea9523a8700447
yukiishida <amusing365@gmail.com>
2013-11-27 19:58:21 +0900

    Update repub_final.php

--- text/bio_explain/repub_final.php
+++ text/bio_explain/repub_final.php
@@ -5,5 +5,13 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-	リパブリック　ファイナル　本文
+「REPUBLIC VOL.10 - THE FINAL -」<br />
+日程：2012.12.1<br />
+会場：WOMB<br />
+<br />
+前回の「REPUBLIC VOL.9 - 映像作家100人 -」に続いて、渋家メンバーによる全身白タイツを着た人間スクリーンとVideoBomberのアプリを用いたパフォーマンスを行った。大量のコンセントを使用した電気容量計算を行い、プロジェクター持参の観客を野良VJとして参加させ、また、動くスクリーンと称して会場フロア上空に7m四方の白い大布を展開し、各VJが投影した。<br />
+<br />
+DJ：okadada<br />
+VJ(Video Bomber set)：exonemo / yang02 / YAVAO / 彩藤 / としくに<br />
+人間スクリーン：渋家
 </div>
