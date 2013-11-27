25ebb2df6def48e2bd46f911a7bfbb6059c1bbd1
yukiishida <amusing365@gmail.com>
2013-11-28 01:13:17 +0900

    Create viv.php

---
+++ text/bio_explain/viv.php
@@ -0,0 +1,13 @@
+<div id="ex_image">
+	<img class="inner_ex_image" src="./img/exhibition_sum/viv/viv_001.jpg">
+	<img class="inner_ex_image" src="./img/exhibition_sum/viv/viv_002.jpg">
+	<img class="inner_ex_image" src="./img/exhibition_sum/viv/viv_003.jpg">
+	<img class="inner_ex_image" src="./img/exhibition_sum/viv/viv_004.jpg">
+</div>
+<div id="ex_text">
+「VIVA☆校」<br />
+日程：2010.12.27 <br />
+会場：棚ガレリ<br />
+<br />
+私たちは、まずギャラリーとその近くの部屋、双方に監視カメラを設置し、相互に監視し合えるようにしました。そして部屋の扉にはバリケードを築いて出入りを禁じ、渋家メンバーが内部を占拠して日常生活を営むのです。ギャラリーの来場者は、監視カメラと繋がったディスプレイを通じて渋家のダラダラとした日常を覗き見ることになり、同時に渋家メンバーからの視線に晒されることにもなります。（中略）実際に展覧会では、渋家側と来場者との、言語を介さない、身振り手振りによるコミュニケーションが丸一日続けられ、コミュニケーションの根源的な愉悦と困難が同時に経験されることとなりました。（文/中島晴矢）
+</div>
