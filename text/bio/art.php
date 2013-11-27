718b93c814272ffea054e4ea1fc618de20c5d180
yukiishida <amusing365@gmail.com>
2013-11-27 21:56:27 +0900

    Update art.php

--- text/bio/art.php
+++ text/bio/art.php
@@ -1,62 +1,62 @@
 <table>
 <tr>
 <th>2008.10</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/osu.php");'>「お好きなものをお選びください展」/デザインフェスタギャラリー（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/osu.php");'>「お好きなものをお選びください展」/デザインフェスタギャラリー</td>
 </tr>
 <tr>
 <th>2009.02</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/rin.php");'>「リンカイ展」/多摩美術大学上野毛キャンパス（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/rin.php");'>「リンカイ展」/多摩美術大学上野毛キャンパス</td>
 </tr>
 <tr>
 <th>2010.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hur.php");'>「フラグメントシアター/断片化の解消」/3331 Arts Chiyoda（パフォーマンス出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hur.php");'>「フラグメントシアター/断片化の解消」/3331 Arts Chiyoda</td>
 </tr>
 <tr>
 <th>2010.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/bar.php");'>「BAR☆Time Line」/3331 Arts Chiyoda（パフォーマンス出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/bar.php");'>「BAR☆Time Line」/3331 Arts Chiyoda</td>
 </tr>
 <tr>
 <th>2010.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tri.php");'>「渋家トリエンナーレ」/3rd house（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tri.php");'>「渋家トリエンナーレ」/3rd house</td>
 </tr>
 <tr>
 <th>2010.12</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/viv.php");'>「VIVA☆校」/棚ガレリ（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/viv.php");'>「VIVA☆校」/棚ガレリ</td>
 </tr>
 <tr>
 <th>2010.12</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/via.php");'>「via art 2010」/シンワアートミュージアム（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/via.php");'>「via art 2010」/シンワアートミュージアム</td>
 </tr>
 <tr>
 <th>2011.01</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/wak.php");'>「わくわく SHIBUYA」/TOKYO WONDER SITE（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/wak.php");'>「わくわく SHIBUYA」/TOKYO WONDER SITE</td>
 </tr>
 <tr>
 <th>2011.10</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/gig.php");'>「ArtGigTokyo2 - 怖いよ、ママー! -」/旧玉井病院（パフォーマンス出演）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/gig.php");'>「ArtGigTokyo2 - 怖いよ、ママー! -」/旧玉井病院</td>
 </tr>
 <tr>
 <th>2011.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ult.php");'>「ULTRA004」/スパイラルガーデン（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ult.php");'>「ULTRA004」/スパイラルガーデン</td>
 </tr>
 <tr>
 <th>2012.05</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/100.php");'>「Village Project HOUSE 100」/The Conteiner（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/100.php");'>「Village Project HOUSE 100」/The Conteiner</td>
 </tr>
 <tr>
 <th>2012.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tra.php");'>「TRANS ARTS TOKYO」/旧東京電機大学校舎11号館（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tra.php");'>「TRANS ARTS TOKYO」/旧東京電機大学校舎11号館</td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/aft.php");'>「アートフェア東京」/東京国際フォーラム（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/aft.php");'>「アートフェア東京」/東京国際フォーラム</td>
 </tr>
 <tr>
 <th>2013.04</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ike.php");'>「渋家「イケる気がする展」- 第一回渋家アート選手権大会 -」/HIGURE 17-15 cas（個展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ike.php");'>「渋家 イケる気がする展」/HIGURE 17-15 cas</td>
 </tr>
 <tr>
 <th>2013.07</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/usn.php");'>「THE NEW BLACK」/Ultra Super New（グループ展）</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/usn.php");'>「THE NEW BLACK」/Ultra Super New</td>
 </tr>
 </table>
