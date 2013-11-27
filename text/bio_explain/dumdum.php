8c45053af96c46407434b27a76f8ca61e96e1097
yukiishida <amusing365@gmail.com>
2013-11-27 19:59:26 +0900

    Update dumdum.php

--- text/bio_explain/dumdum.php
+++ text/bio_explain/dumdum.php
@@ -5,7 +5,13 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-DUM-DUM PARTY2013 <br />
-2013.6.29<br />
-SHIBUYA O-WEST
+「DUM-DUM PARTY 2013」<br />
+日程：2013.6.29<br />
+会場：Shibuya O-WEST<br />
+<br />
+３会場同時に行われた「DUM-DUM PARTY」のオープニングアクトに渋家Exclusiveとして出演し、ちゃんもも◎＋文園太郎＋daokoのライブ、tomadのDJを行った。ちゃんもも◎のライブでは手に装着できるレーザーグローブを制作し、50本のレーザーで会場を盛り上げた。<br />
+<br />
+DJ：tomad<br />
+出演：渋家 / daoko / 文園太郎<br />
+Laser grove：huez ＋ 毒キノコピンク
 </div>
