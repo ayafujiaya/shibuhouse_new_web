bde604c4c334aa85f9f31536c670b9385e138141
yukiishida <amusing365@gmail.com>
2013-11-28 00:57:27 +0900

    Create 5th.php

---
+++ text/bio_explain/5th.php
@@ -0,0 +1,10 @@
+<div id="ex_image">
+	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+</div>
+<div id="ex_text">
+「渋家5周年PARTY」<br />
+2.5Dにて渋家設立5周年を祝うイベントを開催した。
+</div>
