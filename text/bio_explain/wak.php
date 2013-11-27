bf4204ec2032dd987385c60e79b301faf0e13efa
yukiishida <amusing365@gmail.com>
2013-11-27 20:16:51 +0900

    Update wak.php

--- text/bio_explain/wak.php
+++ text/bio_explain/wak.php
@@ -5,10 +5,11 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-わくわくＳＨＩＢＵＹＡ<br />
-2011年1月13日～2月13日<br />
-トーキョーワンダーサイト渋谷<br />
-http://www.youtube.com/watch?v=LVY3fDykhVw<br />
+「わくわく SHIBUYA」 <br />
+日程：2011.1.13 - 2.13<br />
+会場：TOKYO WONDER SITE<br />
 <br />
-http://www.youtube.com/watch?v=od3G4HXTR1I
+「わくわく SHIBUYA」 は、アーティストからアーティストへの紹介でどんどん出展者が増えていく展示だ。主催の「わくわくプロジェクト」は、2009年に「別府現代芸術フェスティバル2009 混浴温泉世界」の国内展として開催された「わくわく混浴アパートメント」をきっかけにスタートし、同世代のアーティストたちが表現者としての意思を共有し、日本中、世界中へと文化発信していくことを目的に全国各地でアートによって人と場をつなぐプロジェクトを行っている。この展示では、渋谷を舞台に全国から集結したアーティストが、作品展示や滞在制作、ライブパフォーマンスなどのイベントを繰り広げた。渋家は、蛇口からでてくるミッキーの物真似などのパフォーマンスを行った。<br />
+<br />
+<a href="http://www.youtube.com/watch?v=LVY3fDykhVw" target="_blank">[MOVIE1]</a><a href="http://www.youtube.com/watch?v=od3G4HXTR1I" target="_blank">[MOVIE2]</a>
 </div>
