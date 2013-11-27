8665eb1ef35822bb1b7c172c64f08cfeca64c5cd
yukiishida <amusing365@gmail.com>
2013-11-28 00:54:04 +0900

    Create jirenma.php

---
+++ text/bio_explain/jirenma.php
@@ -0,0 +1,15 @@
+<div id="ex_image">
+	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
+	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+</div>
+<div id="ex_text">
+「ニッポンのジレンマ」(NHK)<br />
+企画名：「新ＴＯＫＹＯ論」<br />
+放送日：2013.11.30<br />
+<br />
+「『1970年以降生まれ』が語る ニッポン・バージョンアップ作戦」をコンセプトにさまざまな分野の若手論客が議論を交わす討論番組。渋家の6代目代表である齋藤桂太が「新ＴＯＫＹＯ論」をテーマにした放送回にアーティストとして出演。<br />
+<br />
+<a href="http://www.nhk.or.jp/jirenma/index.html" target="_blank">[WEB1]</a><a href="http://dilemmaplus.nhk-book.co.jp/talk/5778" target="_blank">[WEB2]</a>
+</div>
