f6f119b8333fb0e35da0810a96f03b2b7da95c9c
yukiishida <amusing365@gmail.com>
2013-11-27 19:50:10 +0900

    Update nation.php

--- text/bio_explain/nation.php
+++ text/bio_explain/nation.php
@@ -5,11 +5,11 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-「house nation」
-日程：2011.2 - 2011.6
-会場：3rd house
-
-最大週1のペースでMaltine Records主催•tomadの協力のもと渋家3rd houseのリビングをクラブ空間として使用しパーティーを開催。また、Ustream放送を行い、現代美術家の松蔭浩之氏や思想家の千葉雅也氏をお呼びしトークイベントを行ったほか、渋家の引っ越しドキュメンタリー映像の鑑賞会、ジュニアアイドルをテーマにした討論会などを行った。
-
+「house nation」<br />
+日程：2011.2 - 2011.6<br />
+会場：3rd house<br />
+<br />
+最大週1のペースでMaltine Records主催•tomadの協力のもと渋家3rd houseのリビングをクラブ空間として使用しパーティーを開催。また、Ustream放送を行い、現代美術家の松蔭浩之氏や思想家の千葉雅也氏をお呼びしトークイベントを行ったほか、渋家の引っ越しドキュメンタリー映像の鑑賞会、ジュニアアイドルをテーマにした討論会などを行った。<br />
+<br />
 <a href="http://housenation150.blogspot.jp/" target="_blank">[Web]</a>
 </div>
