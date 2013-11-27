514e524ba485c485e30f9bd17045a58c58cef866
yukiishida <amusing365@gmail.com>
2013-11-27 20:25:28 +0900

    Update usn.php

--- text/bio_explain/usn.php
+++ text/bio_explain/usn.php
@@ -5,7 +5,9 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-THE NEW BLACK<br />
-UltraSuperNew Gallery<br />
-2013年7月11日～7月26日文
+「THE NEW BLACK」<br />
+日程：2013.7.11 - 7.26<br />
+会場：Ultra Super New<br />
+<br />
+流行発信地•原宿に創設された「Ultra Super New Gallery」は、キュレーターとのコラボレーションによって国内外からセレクトされたアーティスト作品を紹介する。展示されたすべての作品は購入可能。広さ100 平米のこのスペースは、原宿明治通り沿いのUltra Super Newオフィスの地上階に位置している。オープニング展示として、スコットランドの芸術家ジャック&middot;マクリーンと共に、渋家はメンバー3名の作品を出展した。
 </div>
