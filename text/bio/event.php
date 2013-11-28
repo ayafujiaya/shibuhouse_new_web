81265ea8b63a39b7b737f7143a3cd8224517dad4
yavao <oike.masaki@gmail.com>
2013-11-29 08:02:58 +0900

    Update event.php

--- text/bio/event.php
+++ text/bio/event.php
@@ -5,50 +5,50 @@
 </tr>
 <tr>
 <th>2013.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/yami_2.php");'>「インターネットヤミ市2」/カイブツ社</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/yami_2.php");'><div id="bio_background">「インターネットヤミ市2」/カイブツ社</div></td>
 </tr>
 <tr>
 <th>2013.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/dumdum.php");'> 「DUM-DUM PARTY 2013」/Shibuya O-WEST</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/dumdum.php");'><div id="bio_background">「DUM-DUM PARTY 2013」/Shibuya O-WEST</div></td>
 </tr>
 <tr>
 <th>2013.04</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/5th.php");'>「渋家5周年パーティー」/2.5D</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/5th.php");'><div id="bio_background">「渋家5周年パーティー」/2.5D</div></td>
 </tr>
 <tr>
 <th>2012.12</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_final.php");'>「REPUBLIC VOL.10 - THE FINAL -」/WOMB</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_final.php");'><div id="bio_background">「REPUBLIC VOL.10 - THE FINAL -」/WOMB</div></td>
 </tr>
 <tr>
 <th>2012.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/yami_1.php");'>「インターネットヤミ市」/旧東京電機大学校舎11号館</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/yami_1.php");'><div id="bio_background">「インターネットヤミ市」/旧東京電機大学校舎11号館</div></td>
 </tr>
 <tr>
 <th>2012.08</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hm48.php");'>「H&M48 - home party for 48 hours -」/3rd house</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hm48.php");'><div id="bio_background">「H&M48 - home party for 48 hours -」/3rd house</div></td>
 </tr>
 <tr>
 <th>2012.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/future.php");'>「歌舞伎町マルチネフューチャーパーク」/ニュージャパン</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/future.php");'><div id="bio_background">「歌舞伎町マルチネフューチャーパーク」/ニュージャパン</div></td>
 </tr>
 <tr>
 <th>2012.05</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_9.php");'>「REPUBLIC VOL.9 - 映像作家100人 -」/WOMB</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_9.php");'><div id="bio_background">「REPUBLIC VOL.9 - 映像作家100人 -」/WOMB</div></td>
 </tr>
 <tr>
 <th>2012.05</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/esora.php");'>「Shibuhouse live party」/絵空箱</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/esora.php");'><div id="bio_background">「Shibuhouse live party」/絵空箱</div></td>
 </tr>
 <tr>
 <th>2012.04</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/outof.php");'>「OUT OF HOUSE -シャッキシャキのキャベツを添えて-」/amate-raxi</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/outof.php");'><div id="bio_background">「OUT OF HOUSE -シャッキシャキのキャベツを添えて-」/amate-raxi</div></td>
 </tr>
 <tr>
 <th>2011.02</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/nation.php");'>「house nation」/3rd house</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/nation.php");'><div id="bio_background">「house nation」/3rd house</div></td>
 </tr>
 <tr>
 <th>2010.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/amate.php");'>「渋家パーティー」/amate-raxi</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/amate.php");'><div id="bio_background">「渋家パーティー」/amate-raxi</div></td>
 </tr>
 </table>
