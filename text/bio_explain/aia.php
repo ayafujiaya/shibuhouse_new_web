90a72869e227a7cd72bd5b393261f0e253eb0974
yukiishida <amusing365@gmail.com>
2013-11-29 00:15:40 +0900

    Update aia.php

--- text/bio_explain/aia.php
+++ text/bio_explain/aia.php
@@ -1,9 +1,12 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/aia/aia_001.jpg">
 </div>
 <div id="ex_text">
-	art it asiaの記事
+「Art it asia」<br />
+記事名：「渋家」<br />
+掲載日：2012.8.2<br />
+<br />
+日本を含むアジア＝パシフィックを中心に、現代アートの最新情報を紹介する日英バイリンガルの現代アート情報ポータルサイト「Art it asia」。The Conteinerで開催された渋家の個展「Village Project HOUSE 100」のアフターリポートが掲載された。<br />
+<br />
+<a href="http://www.art-it.asia/u/rhqiun/EmvNMksZWJI2DxbGRy4n/" target="_blank">[WEB]</a>
 </div>
