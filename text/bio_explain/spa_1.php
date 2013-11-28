521785735d09523af3f48440f51ae12b2cac8921
yukiishida <amusing365@gmail.com>
2013-11-29 00:00:09 +0900

    Update spa_1.php

--- text/bio_explain/spa_1.php
+++ text/bio_explain/spa_1.php
@@ -1,9 +1,15 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/spa_1/spa_1_001.jpg">
+	<img class="inner_ex_image" src="./img/media_sum/spa_1/spa_1_002.jpg">
+	<img class="inner_ex_image" src="./img/media_sum/spa_1/spa_1_003.jpg">
+	<img class="inner_ex_image" src="./img/media_sum/spa_1/spa_1_004.jpg">
 </div>
 <div id="ex_text">
-	SPA2011.11.1
+『SPA!』<br />
+記事名：「渋谷で家賃 3 万円の快適シェアハウス」<br />
+掲載号：2011.11.1号<br />
+<br />
+「特集 都心部家賃5万円以下の充実ライフ徹底研究」内の記事として掲載される。齋藤桂太の「基本的なルールは、『陰口を叩かない』『避妊をする』『法律を遵守する』という３つだけ。細かいことは言わずユルく暮らしています」という発言のうち“避妊をする”という部分だけが強調され拡散し、良くも悪くも大きな話題となった。<br />
+<br />
+<a href="http://nikkan-spa.jp/79341" target="_blank">[WEB]</a>
 </div>
