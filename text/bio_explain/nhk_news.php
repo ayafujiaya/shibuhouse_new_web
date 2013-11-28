c911dba082e5717ddaf6f67731f3ddae44e4874f
yukiishida <amusing365@gmail.com>
2013-11-29 00:24:46 +0900

    Update nhk_news.php

--- text/bio_explain/nhk_news.php
+++ text/bio_explain/nhk_news.php
@@ -1,9 +1,10 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/nhk_news/nhk_news_001.jpg">
 </div>
 <div id="ex_text">
-	ニュースセブン
+「NHK ニュース7」(NHK)<br />
+企画名：「アートフェア東京」<br />
+放送日：2013.3.22&nbsp;&nbsp;<br />
+<br />
+日本最大のアート見本市「アートフェア東京」に、作られてきたコミュニティやリレーションおよび活動拠点である渋家の所有の問題を取り扱う作品「Owner Change 」をThe Containerより2億5千万円で出品し、出品中に取材された映像が放送された。
 </div>
