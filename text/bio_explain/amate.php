650010e9059757f16bc535b7f6bf0d902a05610c
yavao <oike.masaki@gmail.com>
2013-11-28 19:41:49 +0900

    Update amate.php

--- text/bio_explain/amate.php
+++ text/bio_explain/amate.php
@@ -1,8 +1,8 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/event_sum/amate/amate_001.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/amate/amate_002.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/amate/amate_003.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/amate/amate_004.jpg">
 </div>
 <div id="ex_text">
 「渋家パーティー」<br />
