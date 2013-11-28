f3ebe1f9a43f97c99d452bcaa8a9c772900f0ed7
yukiishida <amusing365@gmail.com>
2013-11-29 00:04:13 +0900

    Update tss.php

--- text/bio_explain/tss.php
+++ text/bio_explain/tss.php
@@ -1,9 +1,10 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/tss/tss_001.jpg">
 </div>
 <div id="ex_text">
-	地域社会圏主義
+『地域社会圏主義』[山本理顕/LIXIL出版] <br />
+記事名：「ルームシェア&times;コーポラティブハウス&times;コミュニティカフェ」<br />
+刊行日：2012.1.10<br />
+<br />
+建築家•山本理顕が「『地域社会圏』構想」を問題提起する書籍。「ルームシェア&times;コーポラティブハウス&times;コミュニティカフェ」と題した、山本理顕と上野千鶴子の対談のなかで「既存の建物のなかで、自生的にはじめた独自の共助けの棲み方」として取り上げれる。
 </div>
