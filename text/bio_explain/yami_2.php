849380c53942e65b0de8a99e57c9e4bdcef283c7
nishidaatsushi226 <nishidaatsushi226@gmail.com>
2013-11-29 03:49:08 +0900

    Update yami_2.php

--- text/bio_explain/yami_2.php
+++ text/bio_explain/yami_2.php
@@ -12,6 +12,7 @@
 <br />
 出店渋家メンバー：YAVAO&nbsp;&nbsp;/ インターネットおじさん（としくに） / まはる / ちゃんもも◎ / 金藤みなみ / 岡村飛龍 / 毒キノコピンク<br />
 <br />
-http://yami1.biz/
+<br />
+<a href="http://yami1.biz/" target="_blank">[WEB]</a>
 <br />
 </div>
