5ed899ce25360329c70f4dfe7188413ba6b41d8d
yukiishida <amusing365@gmail.com>
2013-11-29 00:21:58 +0900

    Update asahi_2013.php

--- text/bio_explain/asahi_2013.php
+++ text/bio_explain/asahi_2013.php
@@ -1,9 +1,12 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/asahi_2013/asahi_2013_001.jpg">
 </div>
 <div id="ex_text">
-	朝日新聞元旦
+『朝日新聞』<br />
+記事名：「ひとつ屋根の下、飛躍もシェア」 <br />
+掲載号：2013.1.1号&nbsp;&nbsp; <br />
+<br />
+元旦の『朝日新聞』文化面に掲載されたシェアハウス記事「ひとつ屋根の下、飛躍もシェア」 のなかで3つのシェアハウスが取り上げられ、そのなかの1つとして渋家が紹介される。現在、渋家以外の2つのシェアハウスは解散してしまった。<br />
+<br />
+<a href="http://www.asahi.com/news/intro/TKY201212310413.html" target="_blank">[WEB]</a>
 </div>
