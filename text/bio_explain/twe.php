9f3a2171635b1891eb9ccc656e17989a82dda1a5
yukiishida <amusing365@gmail.com>
2013-11-28 23:52:31 +0900

    Update twe.php

--- text/bio_explain/twe.php
+++ text/bio_explain/twe.php
@@ -1,9 +1,15 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/twe/twe_001.jpg">
+	<img class="inner_ex_image" src="./img/media_sum/twe/twe_002.jpg">
+	<img class="inner_ex_image" src="./img/media_sum/twe/twe_003.jpg">
+	<img class="inner_ex_image" src="./img/media_sum/twe/twe_004.jpg">
 </div>
 <div id="ex_text">
-	東京ウィークエンダー
+「Tokyo Weekender」<br />
+記事名：「Uncovering a Tokyo discovery」<br />
+掲載日：2011.5.29<br />
+<br />
+日本で最も長い歴史を持つ英字フリーペーパー「Tokyo Weekender」。2nd houseを巨大な布で覆った作品「BAD CROSS」を制作したことにより、引っ越しを余儀なくされた事象を考察する記事が掲載される。<br />
+<br />
+<a href="http://www.tokyoweekender.com/2011/05/uncovering-a-tokyo-discovery/" target="_blank">[WEB]</a>
 </div>
