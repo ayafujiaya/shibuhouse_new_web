98647151dcedacc4a9a9fb61228b87a6e3ae48f6
yavao <oike.masaki@gmail.com>
2013-11-28 20:11:26 +0900

    Update repub_final.php

--- text/bio_explain/repub_final.php
+++ text/bio_explain/repub_final.php
@@ -1,8 +1,8 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/event_sum/repub_final/repub_final_001.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/repub_final/repub_final_002.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/repub_final/repub_final_003.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/repub_final/repub_final_004.jpg">
 </div>
 <div id="ex_text">
 「REPUBLIC VOL.10 - THE FINAL -」<br />
