98d7f588466ba98b45d7275060f78067d7ab009c
yavao <oike.masaki@gmail.com>
2013-11-28 19:59:13 +0900

    Update repub_9.php

--- text/bio_explain/repub_9.php
+++ text/bio_explain/repub_9.php
@@ -1,8 +1,6 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/event_sum/repub_9_001.jpg">
+
 </div>
 <div id="ex_text">
 「REPUBLIC VOL.9 - 映像作家100人 -」<br />
