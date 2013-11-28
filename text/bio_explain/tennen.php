a5606aa02d4cd31973d0f48c2c314038dcc7681f
yukiishida <amusing365@gmail.com>
2013-11-29 00:14:10 +0900

    Update tennen.php

--- text/bio_explain/tennen.php
+++ text/bio_explain/tennen.php
@@ -1,8 +1,5 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/tennen/tennen_001.jpg">
 </div>
 <div id="ex_text">
 『天然生活』<br />
