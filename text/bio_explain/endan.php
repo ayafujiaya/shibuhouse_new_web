34df78ce746db93bedc04f06c41e882f0129ab98
yukiishida <amusing365@gmail.com>
2013-11-29 00:10:09 +0900

    Update endan.php

--- text/bio_explain/endan.php
+++ text/bio_explain/endan.php
@@ -1,9 +1,12 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/endan/endan_001.jpg">
 </div>
 <div id="ex_text">
-	エンダン
+「#エンダン W」(NOTTV)<br />
+企画名：「渋家」<br />
+放送日：2012.6.5 , 6.22<br />
+<br />
+メインMCをやついいちろうが務める生放送のトーク番組。番組のテーマは「ソーシャルの力で様々な人たちの縁をつなぎ、それについてみんなで談じる番組」。6月5日の放送に渋家メンバーから、齋藤桂太、としくに、ちゃんもも◎が出演。さらに6月22日では番組としては異例のスタジオ外中継として渋家からの生放送が行われた。<br />
+<br />
+<a href="http://www.youtube.com/watch?v=3V19MQssT2g&list=PLejCnLp4_vpLt-DAv6ShnTnZXklFjz7DH&index=3" target="_blank">[MOVIE1]</a><a href="http://www.youtube.com/watch?v=ADwNmEwJyCE&list=PLejCnLp4_vpLt-DAv6ShnTnZXklFjz7DH&index=10" target="_blank">[MOVIE2]</a>
 </div>
