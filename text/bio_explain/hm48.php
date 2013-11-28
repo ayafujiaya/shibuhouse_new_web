a77794bd279da53ec89881dd174accbde25019ef
yavao <oike.masaki@gmail.com>
2013-11-28 20:03:40 +0900

    Update hm48.php

--- text/bio_explain/hm48.php
+++ text/bio_explain/hm48.php
@@ -1,8 +1,5 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/event_sum/hm48/hm48_001.jpg">
 </div>
 <div id="ex_text">
 「H&amp;M48 - home party for 48 hours -」<br />
