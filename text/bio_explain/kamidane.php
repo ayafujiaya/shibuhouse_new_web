cca07b8b3a30bf252eb42f07ccee0b6a357492c6
yukiishida <amusing365@gmail.com>
2013-11-29 00:28:58 +0900

    Update kamidane.php

--- text/bio_explain/kamidane.php
+++ text/bio_explain/kamidane.php
@@ -1,9 +1,10 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/kamidane/kamidane_001.jpg">
 </div>
 <div id="ex_text">
-	もはや神ダネ
+「もはや神ダネ!」(フジテレビ)<br />
+企画名：「アーティストの卵が集まるシェアハウスで神ダネ探し」<br />
+放送日：2013.6.13 , 6.20&nbsp;&nbsp; <br />
+<br />
+いじりの神•東野幸治とフォローの神•小籔千豊が全国の街や学校に出没。出会った人たちの中から、将来「何かしらの神になるんじゃないか!? 」と思える（神ダネ＝神の種）を発掘し応援するバラエティー番組。渋家は「アーティストの卵が集まるシェアハウスで神ダネ探し」として、2週にわたって取り上げられた。
 </div>
