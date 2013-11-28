ad18afde22e2f0777fb79378bcbe0154982e4324
yavao <oike.masaki@gmail.com>
2013-11-29 08:08:27 +0900

    Update media.php

--- text/bio/media.php
+++ text/bio/media.php
@@ -1,82 +1,82 @@
 <table>
 <tr>
 <th>2013.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/jirenma.php");'>「ニッポンのジレンマ」[NHK]/「新ＴＯＫＹＯ論」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/jirenma.php");'><div id="bio_background">「ニッポンのジレンマ」[NHK]/「新ＴＯＫＹＯ論」</div></td>
 </tr>
 
 <tr>
 <th>2013.10</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ropponngi_2013.php");'>『六本木クロッシング2013展 アウト・オブ・ダウト』[森美術館]</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ropponngi_2013.php");'><div id="bio_background">『六本木クロッシング2013展 アウト・オブ・ダウト』[森美術館]</div></td>
 </tr>
 <tr>
 <th>2013.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/kamidane.php");'>「もはや神タネ!」[フジテレビ]</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/kamidane.php");'><div id="bio_background">「もはや神タネ!」[フジテレビ]</div></td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/art_it_asia.php");'>「Art it asia」/「売り渋家」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/art_it_asia.php");'><div id="bio_background">「Art it asia」/「売り渋家」</div></td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/25d_news.php");'>「2.5D」/「「渋家」が新メンバー募集、新料金は 2.5 万」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/25d_news.php");'><div id="bio_background">「2.5D」/「「渋家」が新メンバー募集、新料金は 2.5 万」</div></td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/nhk_news.php");'>「NHKニュース7」[NHK]/「アートフェア東京」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/nhk_news.php");'><div id="bio_background">「NHKニュース7」[NHK]/「アートフェア東京」</div></td>
 </tr>
 <tr>
 <th>2013.01</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/asahi_2013.php");'>『朝日新聞』/「ひとつ屋根の下、飛躍もシェア」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/asahi_2013.php");'><div id="bio_background">『朝日新聞』/「ひとつ屋根の下、飛躍もシェア」</div></td>
 </tr>
 <tr>
 <th>2012.09</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/netokaru_1.php");'>「NETOKARU」/「“渋家”新代表に菅井早苗が就任」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/netokaru_1.php");'><div id="bio_background">「NETOKARU」/「“渋家”新代表に菅井早苗が就任」</div></td>
 </tr>
 <tr>
 <th>2012.08</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/aia.php");'>「Art it asia」/「渋家」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/aia.php");'><div id="bio_background">「Art it asia」/「渋家」</div></td>
 </tr>
 <tr>
 <th>2012.07</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/bbc.php");'>「Japan, Fall of the Rising Sun」[BBC Three]</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/bbc.php");'><div id="bio_background">「Japan, Fall of the Rising Sun」[BBC Three]</div></td>
 </tr>
 <tr>
 <th>2012.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/endan.php");'>「#エンダン W」[NOTTV]</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/endan.php");'><div id="bio_background">「#エンダン W」[NOTTV]</div></td>
 </tr>
 
 <tr>
 <th>2012.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tennen.php");'>『天然生活』/「物々交換を開催してみました」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tennen.php");'><div id="bio_background">『天然生活』/「物々交換を開催してみました」</div></td>
 </tr>
 
 <tr>
 <th>2012.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/artguide.php");'>『東京アートガイド』[美術手帖編集部]</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/artguide.php");'><div id="bio_background">『東京アートガイド』[美術手帖編集部]</div></td>
 </tr>
 <tr>
 <th>2012.01</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tss.php");'>『地域社会圏主義』[山本理顕]</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tss.php");'><div id="bio_background">『地域社会圏主義』[山本理顕]</div></td>
 </tr>
 <tr>
 <th>2011.12</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/dommune.php");'>『DOMMUNE オフィシャルガイドブック 2』[DOMMUNE]</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/dommune.php");'><div id="bio_background">『DOMMUNE オフィシャルガイドブック 2』[DOMMUNE]</div></td>
 </tr>
 <tr>
 <th>2011.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hk.php");'>『HK 2』/「特集 さあ、〈一緒に〉暮そう」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hk.php");'><div id="bio_background">『HK 2』/「特集 さあ、〈一緒に〉暮そう」</div></td>
 </tr>
 <tr>
 <th>2011.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/spa_1.php");'>『SPA!』/「渋谷で家賃 3 万円の快適シェアハウス」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/spa_1.php");'><div id="bio_background">『SPA!』/「渋谷で家賃 3 万円の快適シェアハウス」</div></td>
 </tr>
 <tr>
 <th>2011.08</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/gaget_1.php");'>「ガジェット通信」/「シェアハウス”渋家”の過去・現在・未来」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/gaget_1.php");'><div id="bio_background">「ガジェット通信」/「シェアハウス”渋家”の過去・現在・未来」</div></td>
 </tr>
 <tr>
 <th>2011.05</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/twe.php");'>「Tokyo Weekender」/「Uncovering a Tokyo discovery」</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/twe.php");'><div id="bio_background">「Tokyo Weekender」/「Uncovering a Tokyo discovery」</div></td>
 </tr>
 <tr>
 <th>2011.03</th>
