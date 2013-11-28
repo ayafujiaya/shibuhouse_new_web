bfaac966aae6af47a25ab62d2246d8eece88297c
yukiishida <amusing365@gmail.com>
2013-11-28 21:20:22 +0900

    Update member.php

--- member.php
+++ member.php
@@ -483,8 +483,12 @@
 						<a  class="profile_name" title="増沢大輝
 						<div class='explain'>
 							<h1>ダンサー・振付家役</h1>
-							<div class='explain_text'>1989年生まれ。4人兄弟の真ん中の下の方という絶妙なタイミングで生まれ自由奔放に育てられる。 16歳のときBREAK DANCEを入り口に路上で夜な夜な踊り始め、１年後に創作ダンスをはじめる。 HIPHOP、ロック、舞
-							踏、コンテンポラリーなど広めのジャンルを浅めに経由しつつ独自のスタイルを模索中。</br>
+							<div class='explain_text'>ダンサー・振付家<br />
+<br />
+1989年生まれ。<br />
+ストリートダンスを入り口に広めのジャンルを浅めに経由。「今の中にしかない身体」をテーマにそれが持つ「ずるさ」をダンスとして、抱える「切実さ」はパフォーミングアーツとして分類し活動を展開中。現在は身体のアーカイブ化にも取り組む。<br />
+「<a href="http://www.youtube.com/watch?v=ZRM08-P79tQ" target="_blank">cinema is dancing</a>」<br />
+「<a href="http://m.youtube.com/watch?v=OhMdGUjb8JI&amp;desktop_uri=%2Fwatch%3Fv%3DOhMdGUjb8JI" target="_blank">penetration twin</a>」</br>
 							</br>
 							<a href='http://twitter.com/taiki02'><img src='./img/twitter.png'  border='0'></a>
 							&nbsp;
