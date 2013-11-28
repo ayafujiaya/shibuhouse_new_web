92aa9a29f332e48eb424e2497ea89ff869c6de3c
yukiishida <amusing365@gmail.com>
2013-11-29 00:01:53 +0900

    Update hk.php

--- text/bio_explain/hk.php
+++ text/bio_explain/hk.php
@@ -1,9 +1,10 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/hk/hk_001.jpg">
 </div>
 <div id="ex_text">
-	HK2
+『HK 2』<br />
+記事名：「特集 さあ、〈いっしょに〉暮そう」<br />
+刊行日：2011.11.3<br />
+<br />
+インディーズで刊行される文芸・カルチャーマガジン「HK」。 渋家へ1週間泊まり込みを行ったルポールタージュが掲載される。現在のところ渋家を取り上げた記事では最長の文章量。過激な全裸集合写真が表紙に使用され話題となり即売会では即日完売となった。
 </div>
