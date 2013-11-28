06652cf122beea5ec8d7b79920e2cde543d6a545
yukiishida <amusing365@gmail.com>
2013-11-28 23:50:33 +0900

    Update asahi_j.php

--- text/bio_explain/asahi_j.php
+++ text/bio_explain/asahi_j.php
@@ -1,9 +1,13 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/asahi_j/asahi_j_001.jpg">
+	<img class="inner_ex_image" src="./img/media_sum/asahi_j/asahi_j_002.jpg">
+	<img class="inner_ex_image" src="./img/media_sum/asahi_j/asahi_j_003.jpg">
+	<img class="inner_ex_image" src="./img/media_sum/asahi_j/asahi_j_004.jpg">
 </div>
 <div id="ex_text">
-	朝日ジャーナル
+『朝日ジャーナル』<br />
+記事名：「特集&nbsp;&nbsp;日本破壊計画 – シェアハウスの人々 -」<br />
+掲載号：2011.3.19号<br />
+<br />
+発刊直前に大震災が起き、特集タイトルが問題視された『朝日ジャーナル』復刊号。『朝日ジャーナル』は、1960年代から1970年代に隆盛をきわめた雑誌であり「怒りの復活」と銘打たれた復刊号には、格差や貧困に象徴される、日本型社会システムの破綻をテーマに、さまざまな論客が論考を寄せた。 渋家は4Pにわたるルポルタージュが掲載される。
 </div>
