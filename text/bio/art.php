6d30f08808bef700cb18750b18d9214da025a5a0
yavao <oike.masaki@gmail.com>
2013-11-29 08:04:50 +0900

    Update art.php

--- text/bio/art.php
+++ text/bio/art.php
@@ -1,59 +1,59 @@
 <table>
 <tr>
 <th>2013.07</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/usn.php");'>「THE NEW BLACK」/Ultra Super New</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/usn.php");'><div id="bio_background">「THE NEW BLACK」/Ultra Super New</div></td>
 </tr>
 <tr>
 <th>2013.04</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ike.php");'>「渋家 イケる気がする展」/HIGURE 17-15 cas</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ike.php");'><div id="bio_background">「渋家 イケる気がする展」/HIGURE 17-15 cas</div></td>
 </tr>
 <tr>
 <th>2013.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/aft.php");'>「アートフェア東京」/東京国際フォーラム</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/aft.php");'><div id="bio_background">「アートフェア東京」/東京国際フォーラム</div></td>
 </tr>
 <tr>
 <th>2012.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tra.php");'>「TRANS ARTS TOKYO」/旧東京電機大学校舎11号館</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tra.php");'><div id="bio_background">「TRANS ARTS TOKYO」/旧東京電機大学校舎11号館</div></td>
 </tr>
 <tr>
 <th>2012.05</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/100.php");'>「Village Project HOUSE 100」/The Conteiner</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/100.php");'><div id="bio_background">「Village Project HOUSE 100」/The Conteiner</div></td>
 </tr>
 <tr>
 <th>2011.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ult.php");'>「ULTRA004」/スパイラルガーデン</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/ult.php");'><div id="bio_background">「ULTRA004」/スパイラルガーデン</div></td>
 </tr>
 <tr>
 <th>2011.10</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/gig.php");'>「ArtGigTokyo2 - 怖いよ、ママー! -」/旧玉井病院</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/gig.php");'><div id="bio_background">「ArtGigTokyo2 - 怖いよ、ママー! -」/旧玉井病院</div></td>
 </tr>
 <tr>
 <th>2011.01</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/wak.php");'>「わくわく SHIBUYA」/TOKYO WONDER SITE</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/wak.php");'><div id="bio_background">「わくわく SHIBUYA」/TOKYO WONDER SITE</div></td>
 </tr>
 <tr>
 <th>2010.12</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/viv.php");'>「VIVA☆校」/棚ガレリ</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/viv.php");'><div id="bio_background">「VIVA☆校」/棚ガレリ</div></td>
 </tr>
 <tr>
 <th>2010.12</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/via.php");'>「via art 2010」/シンワアートミュージアム</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/via.php");'><div id="bio_background">「via art 2010」/シンワアートミュージアム</div></td>
 </tr>
 <tr>
 <th>2010.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tri.php");'>「渋家トリエンナーレ」/3rd house</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/tri.php");'><div id="bio_background">「渋家トリエンナーレ」/3rd house</div></td>
 </tr>
 <tr>
 <th>2010.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/bar.php");'>「BAR☆Time Line」/3331 Arts Chiyoda</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/bar.php");'><div id="bio_background">「BAR☆Time Line」/3331 Arts Chiyoda<</div>/td>
 </tr>
 <tr>
 <th>2010.03</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hur.php");'>「フラグメントシアター/断片化の解消」/3331 Arts Chiyoda</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hur.php");'><div id="bio_background">「フラグメントシアター/断片化の解消」/3331 Arts Chiyoda</div></td>
 </tr>
 <tr>
 <th>2009.02</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/rin.php");'>「リンカイ展」/多摩美術大学上野毛キャンパス</td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/rin.php");'><div id="bio_background">「リンカイ展」/多摩美術大学上野毛キャンパス</div></td>
 </tr>
 <tr>
 <th>2008.10</th>
