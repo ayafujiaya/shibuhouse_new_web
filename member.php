38d7b601686acd70d739db17743fa2aa8b17bdee
yavao <oike.masaki@gmail.com>
2013-11-28 15:54:07 +0900

    Update member.php

--- member.php
+++ member.php
@@ -62,9 +62,9 @@
 							、「人と人のコミュニケーションからコンテンツを生み出す文化事業」をコンセプトに、そこに集うメンバーと共に様々
 							な作品、企画、展示、イベント、プロジェクトなどを製作している。</br>
 							
-							<a href='https://twitter.com/satoketa'><img src="./img/twitter_header.png" border="0"></a></div>
+							<a href='https://twitter.com/satoketa'><img src='./img/twitter_header.png' border='0'></a></div>
 						</div>
-						" data-lightbox="roadtrip" href="./img/menber_sum/Keita_saito.jpg" style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
+						" data-lightbox="roadtrip" href='./img/menber_sum/Keita_saito.jpg' style="display:block; width:165px; height:247px; position:absolute; top:0px; left:0px;">
 						<div>
 							<p class="full_name">Mouse Over Name</p>
 						</div>
