94d7948a49e24d4eefa4af44cf665c8199e33f60
nishidaatsushi226 <nishidaatsushi226@gmail.com>
2013-11-28 19:42:37 +0900

    Update event.php

--- text/bio/event.php
+++ text/bio/event.php
@@ -1,58 +1,56 @@
 <table>
 <tr>
-<th>2010.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/amate.php");'>「渋家パーティー」/amate-raxi</td>
-</tr>
-<tr>
-<th>2011.02</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/nation.php");'>「house nation」/3rd house</td>
-</tr>
-<tr>
-<th>2011.04</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/outof.php");'>「OUT OF HOUSE -シャッキシャキのキャベツを添えて-」/amate-raxi</td>
-</tr>
-<tr>
-<th>2012.05</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/esora.php");'>「Shibuhouse live party」/絵空箱</td>
-</tr>
-<tr>
-<th>2012.05</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_9.php");'>「REPUBLIC VOL.9 - 映像作家100人 -」/WOMB</td>
-</tr>
-<tr>
-<th>2012.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/future.php");'>「歌舞伎町マルチネフューチャーパーク」/ニュージャパン</td>
-</tr>
-<tr>
-<th>2012.08</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hm48.php");'>「H&M48 - home party for 48 hours -」/3rd house</td>
-</tr>
-<tr>
-<th>2012.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/yami_1.php");'>「インターネットヤミ市」/旧東京電機大学校舎11号館</td>
-</tr>
-
-
-<tr>
-<th>2013.4</th>
-<td style="cursor: pointer" onclick=''>「渋家5周年パーティー」/2.5D</td>
-</tr>
-
-
-<tr>
-<th>2012.12</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_final.php");'>「REPUBLIC VOL.10 - THE FINAL -」/WOMB</td>
-</tr>
-<tr>
-<th>2013.06</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/dumdum.php");'> 「DUM-DUM PARTY 2013」/Shibuya O-WEST</td>
+<th>2013.10</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/vivivi.php");'>「VIVIVI」/Club Asia</td>
 </tr>
 <tr>
 <th>2013.06</th>
 <td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/yami_2.php");'>「インターネットヤミ市2」/カイブツ社</td>
 </tr>
 <tr>
-<th>2013.10</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/vivivi.php");'>「VIVIVI」/Club Asia</td>
+<th>2013.06</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/dumdum.php");'> 「DUM-DUM PARTY 2013」/Shibuya O-WEST</td>
+</tr>
+<tr>
+<th>2012.12</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_final.php");'>「REPUBLIC VOL.10 - THE FINAL -」/WOMB</td>
+</tr>
+
+<tr>
+<th>2013.4</th>
+<td style="cursor: pointer" onclick=''>「渋家5周年パーティー」/2.5D</td>
+</tr>
+
+<tr>
+<th>2012.11</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/yami_1.php");'>「インターネットヤミ市」/旧東京電機大学校舎11号館</td>
+</tr>
+<tr>
+<th>2012.08</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/hm48.php");'>「H&M48 - home party for 48 hours -」/3rd house</td>
+</tr>
+<tr>
+<th>2012.06</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/future.php");'>「歌舞伎町マルチネフューチャーパーク」/ニュージャパン</td>
+</tr>
+<tr>
+<th>2012.05</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/repub_9.php");'>「REPUBLIC VOL.9 - 映像作家100人 -」/WOMB</td>
+</tr>
+<tr>
+<th>2012.05</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/esora.php");'>「Shibuhouse live party」/絵空箱</td>
+</tr>
+<tr>
+<th>2011.02</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/nation.php");'>「house nation」/3rd house</td>
+</tr>
+<tr>
+<th>2012.05</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/esora.php");'>「Shibuhouse live party」/絵空箱</td>
+</tr>
+<tr>
+<th>2010.06</th>
+<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/amate.php");'>「渋家パーティー」/amate-raxi</td>
 </tr>
 </table>
