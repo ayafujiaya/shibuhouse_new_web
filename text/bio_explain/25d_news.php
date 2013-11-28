c70d8612f281ae786304b4c4a0ecea2c4f9745e8
yukiishida <amusing365@gmail.com>
2013-11-29 00:26:12 +0900

    Update 25d_news.php

--- text/bio_explain/25d_news.php
+++ text/bio_explain/25d_news.php
@@ -1,9 +1,12 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/25d_news/25d_news_001.jpg">
 </div>
 <div id="ex_text">
-	2.5D新メンバー
+「2.5D」<br />
+記事名：「「渋家」が新メンバー募集、新料金は 2.5 万」<br />
+掲載日：2013.3.13<br />
+<br />
+4月から加入する「新メンバー募集」をメディアの協力のもと展開。参加条件は「単に居住だけでなく、一緒にオルタナティブスペースを運営していくという意味も込めて、住人ではなくメンバーを募集する」と意図的に厳しく設定したが、結果、メンバー数は24名から36名へと増えた。<br />
+<br />
+<a href="http://2-5-d.jp/news/5670/" target="_blank">[WEB]</a>
 </div>
