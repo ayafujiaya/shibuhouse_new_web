3be5c1f7c87fec75819aca438c72ceb1d7c57447
yavao <oike.masaki@gmail.com>
2013-11-28 20:00:39 +0900

    Update future.php

--- text/bio_explain/future.php
+++ text/bio_explain/future.php
@@ -1,8 +1,8 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/event_sum/future_001.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/future_002.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/future_003.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/future_004.jpg">
 </div>
 <div id="ex_text">
 「歌舞伎町マルチネフューチャーパーク」<br />
