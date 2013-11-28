bde34c30fc05e7927fa19b47a4d6097f02c06eb9
yavao <oike.masaki@gmail.com>
2013-11-28 20:05:23 +0900

    Update yami_1.php

--- text/bio_explain/yami_1.php
+++ text/bio_explain/yami_1.php
@@ -1,8 +1,8 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/event_sum/yami_1/yami_1_001.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/yami_1/yami_1_002.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/yami_1/yami_1_003.jpg">
+	<img class="inner_ex_image" src="./img/event_sum/yami_1/yami_1_004.jpg">
 </div>
 <div id="ex_text">
 「インターネットヤミ市」<br />
