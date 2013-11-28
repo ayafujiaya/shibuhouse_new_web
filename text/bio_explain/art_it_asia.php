f435f44b71644ed368743f0afd8680de670b29a4
yukiishida <amusing365@gmail.com>
2013-11-29 00:27:03 +0900

    Update art_it_asia.php

--- text/bio_explain/art_it_asia.php
+++ text/bio_explain/art_it_asia.php
@@ -5,5 +5,11 @@
 	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
 </div>
 <div id="ex_text">
-	art it asia 売り渋家
+「Art it asia」<br />
+記事名：「売り渋家」<br />
+掲載日：2013.3.17<br />
+<br />
+渋家が「アートフェア東京2013」に出品した、作られてきたコミュニティやリレーションおよび活動拠点である渋家の所有の問題を取り扱う作品「Owner Change 」が、2億5千万円という販売額であることが議論された事象が取り上げられる。<br />
+<br />
+<a href="http://www.art-it.asia/u/rhqiun/G1vXMLQNib9pWTSetDfC/" target="_blank">[WEB]</a>
 </div>
