e78bd3f6da233c033a769bcf788ba8564114896f
yukiishida <amusing365@gmail.com>
2013-11-29 00:17:11 +0900

    Update bbc.php

--- text/bio_explain/bbc.php
+++ text/bio_explain/bbc.php
@@ -1,9 +1,12 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/bbc/bbc_001.jpg">
 </div>
 <div id="ex_text">
-	BBC japan,fall~
+「Japan, Fall of the Rising Sun」(BBC Three) <br />
+企画名：「Life in Tokyo: 20 People Living in 1 House!」<br />
+放送日：2012.7.17<br />
+<br />
+英国の国営放送のBBCが制作•放送を行ったドキュメンタリー番組。取材班は、実際に渋家に訪れ、 メンバーにインタビューを行ったほか、毎月22日開催の「渋家ホームパーティ」にも参加した。<br />
+<br />
+<a href="http://www.bbc.co.uk/programmes/b01knsnp" target="_blank">[WEB]</a>
 </div>
