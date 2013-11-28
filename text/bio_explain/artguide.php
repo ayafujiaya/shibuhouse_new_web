f4468d5217ca29d814bf8289ae6e596a837fefdb
yukiishida <amusing365@gmail.com>
2013-11-29 00:07:46 +0900

    Update artguide.php

--- text/bio_explain/artguide.php
+++ text/bio_explain/artguide.php
@@ -1,9 +1,10 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/artguide/artguide_001.jpg">
 </div>
 <div id="ex_text">
-	東京アートガイド
+『東京アートガイド』[美術手帖編集部/美術出版社]<br />
+記事名：「エリア別アートスポット完全ガイド」<br />
+刊行日：2012.3.18<br />
+<br />
+『美術手帖』編集部が注目するアートスポット450件が完全網羅された書籍。そのなかの1つとして「24時間365日解放しておくことで、新たな関係性を生み出していくアートスペース」と紹介される。
 </div>
