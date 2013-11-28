94fc7b43927d8db24571702202129c9f987d3b32
nishidaatsushi226 <nishidaatsushi226@gmail.com>
2013-11-28 17:16:19 +0900

    Update hm48.php

--- text/bio_explain/hm48.php
+++ text/bio_explain/hm48.php
@@ -11,49 +11,8 @@
 <br />
 さまざまなDJ、VJを呼んで渋家地下にある12畳のイベントスペース•クヌギにて48時間連続のクラブイベントを行った。屋上にはプールを設置。
 <br />
-21:00@chefoba<br />
-21:40@Dan_chooooooo<br />
-22:20 Silvanian Families<br />
-23:00&nbsp; @yako_flapper3<br />
-2012年 8月 4日 (土曜日)<br />
-00:00@syun_chang<br />
-02:00&nbsp; @junkMa<br />
-03:00&nbsp; @tomad<br />
-04:00&nbsp; mirrorball inferno ( @tamio @e2boye4boy )<br />
-05:00&nbsp; 建設中 (@nnnnnnnnnnn @Geak_Boy @hugabooks)<br />
-08:00&nbsp; DJ換気<br />
-10:00&nbsp; @moriura (DJMIX)<br />
-12:00&nbsp; DEMP@HOLIC<br />
-16:00&nbsp; DJ換気<br />
-17:00&nbsp; @GoddoSukoupion<br />
-18:00&nbsp; ヤバい中村おじトマド ( @kusokuniman @tomad @syuto_nakamura )<br />
-19:00&nbsp; @inu<br />
-20:00&nbsp; @yosshibox<br />
-21:00&nbsp; @soulflower_no1 / @satononono<br />
-22:30&nbsp; @Licaxxx<br />
-23:15&nbsp; Zwei Raketen ( @yoshiki_zr @IVOZ_ZR )<br />
-<br />
-2012年 8月 5日 (日曜日)<br />
-00:00&nbsp; MIXXXTURE(@futatsuki @skiemo1651 @deepriver598)+@seimei1992<br />
-03:00&nbsp; @sskhybrid<br />
-04:00&nbsp; NO DISCO RECORDS<br />
-05:00&nbsp; @Not1nservice<br />
-06:00&nbsp; @Yanmar21<br />
-07:00&nbsp;DJ 換気<br />
-08:00&nbsp; @ishikur0 (UST/ナハウス)<br />
-09:00&nbsp; @hotori<br />
-10:00&nbsp; @thommole<br />
-11:00&nbsp; DJ クリとリス @satoketa<br />
-11:45&nbsp; @hnnhn<br />
-12:30&nbsp; @kusokuniman<br />
-13:15 もかもか<br />
-14:00&nbsp; コバルト爆弾αΩ<br />
-15:00&nbsp; @Miiixn<br />
-16:00&nbsp; @akinyan_75<br />
-17:00&nbsp; @kalapattar<br />
-18:00&nbsp; DJ USYN<br />
-19:00&nbsp; @rnts<br />
-20:00&nbsp; DJ WILDPARTY<br />
+DJ：chefoba / Dan_chooooooo / Silvanian Families / yako_flapper3 / syun_chang / junkMa / tomad / mirrorball inferno / 建設中 / 換気 / moriura / DEMP / 換気 / GoddoSukoupion / ヤバい中村おじトマド / inu / yosshibox / soulflower_no1 / satononono / Licaxxx / Zwei Raketen / MIXXXTURE+seimei1992 / sskhybrid / NO DISCO RECORDS / Not1nservice / Yanmar21 / ishikur0 / hotori / thommole / DJ クリスマス / hnnhn / kusokuniman / もかもか / コバルト爆弾αΩ / Miiixn / akinyan_75 / kalapattar / DJ USYN / rnts / DJ WILDPARTY
+VJ：huez / hsgn / YAVAO / yang02 / GraphersRock / 換気 / yuichibass / U-ji a.k.a 霊長類P/ ホンマカズキ / Ishizawa / yumelight / yumelight+YAVAO / ざき / m7kenji / futatuki / sskhybrid / CashmereBear / ナハウスUST / hamarooo / yuichibass / 吉川マッハスペシャル / daahara / blok m / hrk0613 / ハラダアツシ; @Licaxxx<br />
 <br />
 <br />
 <br />
