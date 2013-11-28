e41cf45e5e780ffb215d494fd19508268e94a8f4
yukiishida <amusing365@gmail.com>
2013-11-29 00:30:25 +0900

    Update ropponngi_2013.php

--- text/bio_explain/ropponngi_2013.php
+++ text/bio_explain/ropponngi_2013.php
@@ -1,9 +1,12 @@
 <div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
+	<img class="inner_ex_image" src="./img/media_sum/ropponngi/roppnngi.jpg">
 </div>
 <div id="ex_text">
-	六本木クロッシング
+『六本木クロッシング2013展 アウト・オブ・ダウト 来たるべき風景のために』[森美術館/平凡社] <br />
+記事名：「ディスカーシブ・プラットホーム」<br />
+刊行日：2013.10.28 <br />
+<br />
+森美術館が3年に一度開催する現代アートの展覧会「六本木クロッシング」の公式カタログ。渋家は「六本木クロッシング」のなかで「全国のディスカーシブ・プラットホーム」の1つとして選出されており、本書でも同様の項目で取り上げられた。<br />
+<br />
+<a href="http://www.mori.art.museum/jp" target="_blank">[WEB]</a>
 </div>
