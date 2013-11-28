80b575b994904a3bf654a57c1ea83c2fe49db681
yukiishida <amusing365@gmail.com>
2013-11-29 00:20:02 +0900

    Update and rename nerokaru_1.php to netokaru_1.php

--- text/bio_explain/nerokaru_1.php
+++
@@ -1,9 +0,0 @@
-<div id="ex_image">
-	<img class="inner_ex_image" src="./img/AI/dammy_01.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_02.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_03.gif">
-	<img class="inner_ex_image" src="./img/AI/dammy_04.gif">
-</div>
-<div id="ex_text">
-	寝とかるニュース
-</div>
---
+++ text/bio_explain/netokaru_1.php
@@ -0,0 +1,12 @@
+<div id="ex_image">
+	<img class="inner_ex_image" src="./img/media_sum/netokaru_1/netokaru_1_001.jpg">
+</div>
+<div id="ex_text">
+「NETOKARU」<br />
+記事名：「渋谷にあるオルタナティブスペース”渋家”新代表に菅井早苗が就任」 <br />
+掲載日：2012.9.22 <br />
+<br />
+渋家の5代目代表に菅井早苗が就任したことを記念して行われたインタビュー記事。菅井は「これからの渋家は今までにない女性的な文化を取り入れていこうと思ってます」とこれまでの渋家のイメージを一新させる意気込みを語り、そのイメージは現在の6代目代表の渋家へと引き継がれている。<br />
+<br />
+<a href="http://netokaru.com/?p=13665" target="_blank">[WEB]</a>
+</div>
