4bb9e8448a9c78e585e69d752a183f2f564f530c
ayafuji <ayafuji.aya@gmail.com>
2013-11-29 21:13:10 +0900

    Update member.php

--- member.php
+++ member.php
@@ -180,13 +180,12 @@ See below for details.</br>
             <div class='explain'>
               <h1>Interactive Media Engineer / Burner(Burningman 2012)</h1>
               <div class='explain_text'>
-		人とシステムとの間の関係性に着目し、その中での「相互作用性」をテーマに、web, ハードウェア, VJアプリケーション等、多岐にわたるメディアでのクリエイティブなシステムを\\
-作り続けている。フリーランスのエンジニアとして活動する傍ら、表現者として作品作りも行う。代表作に、ステージと観客との関係性に着目した「ofxHuez」, 建築とそこに住まう人々との関係性に\\
+		人とシステムとの間の関係性に着目し、その中での「相互作用性」をテーマに、web, ハードウェア, VJアプリケーション等、多岐にわたるメディアでのクリエイティブなシステムを
+作り続けている。フリーランスのエンジニアとして活動する傍ら、表現者として作品作りも行う。代表作に、ステージと観客との関係性に着目した「ofxHuez」, 建築とそこに住まう人々との関係性に
 着目した「House API Project」がある。
                 <hr>
                 VJアプリケーション</br>
                 <a href='https://www.youtube.com/watch?v=4tcx01-wffs'>ofxHuez</a>
-                <img style='width:370px;' src='./img/member_sum/ofxhuez.png'>
                 <br>
                 <br>
                 House API Project<br>
