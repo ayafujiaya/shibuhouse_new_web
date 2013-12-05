33906ade942c42e272c95ec1fa66acd67d36ec70
shibuhouse <shibuhouse@www14155uj.sakura.ne.jp>
2013-12-05 17:44:04 +0900

    add

--- text/bio_explain/usn.php
+++ text/bio_explain/usn.php
@@ -1,16 +1,22 @@
 
+
+<!-- ここから -->
 <?php 
-var full_image = './img/exhibition_full/usn/usn_002.jpg';
-var sum_image = './img/exhibition_sum/usn/usn_001.jpg';
-var comment = 'ultra super new!!';
+#ポップアップ後の画像の位置
+$full_image = './img/exhibition_full/usn/usn_002.jpg';
+#ポップアップ前の画像の位置
+$sum_image = './img/exhibition_sum/usn/usn_001.jpg';
+#ポップアップ後のコメントの内容(空でも良い)
+$comment = 'ultra super new!!';
 ?>
 <div id="ex_image">
   <!-- hrefの部分にポップアップ後の画像のパスを書く-->
-  <a name="<?php echo comment ?>" rel="lightbox[imagegroup]" data-lightbox="roadtrip" href="<?php echo full_image ?>">
+  <a name="<?php echo $comment ?>" rel="lightbox[imagegroup]" data-lightbox="roadtrip" href="<?php echo $full_image ?>">
     <!-- テキストでも画像でもよいけど、<a>タグでポップアップのために押させたいボタンを囲んであげる。 -->
-    <img class="inner_ex_image" src="<?php echo sum_image ?>">
+    <img class="inner_ex_image" src="<?php echo $sum_image ?>">
   </a><!-- aタグ締め -->
 </div>
+<!-- ここまでをテンプレートとして使う -->
 
 <div id="ex_text">
 「THE NEW BLACK」<br />
