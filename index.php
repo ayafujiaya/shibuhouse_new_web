41ba94e7b5596cc307bd33c8cdfffd9384f71367
yukiishida <amusing365@gmail.com>
2014-02-27 06:24:45 +0900

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
         <div class="flexslider">
           <ul class="slides">
             <li><a target="_blank" href="http://shibuhouse-image-fes.tumblr.com/"><img src="./img/topimage/eizo.jpg"></a></li>
-            <li><a target="_blank" href="https://www.facebook.com/events/1390855861175162/?ref=2&ref_dashboard_filter=upcoming"><img src="./img/topimage/22paty.jpg"></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/events/339720959504482/"><img src="./img/topimage/home_party_icon.jpg"></a></li>
             <li><a target="_blank" href="https://www.facebook.com/events/203896739818170/?ref_dashboard_filter=upcoming"><img src="./img/topimage/shibuhouseart.jpg"></a></li>
             <li><a target="_blank" href="https://www.facebook.com/events/737247209621552/?ref=2&ref_dashboard_filter=upcoming"><img src="./img/topimage/chihou.jpg"></a></li>
             
