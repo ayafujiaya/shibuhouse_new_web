2443837ca872234daea0273c20b1c8b0163729f4
nishidaatsushi226 <nishidaatsushi226@gmail.com>
2014-04-08 17:11:24 +0900

    Update media.php

--- text/bio/media.php
+++ text/bio/media.php
@@ -1,19 +1,19 @@
 <table>
 <tr>
 <th>2014.02</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/jirenma.php");'><div id="bio_background">『中庭』[中庭編集部]</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("");'><div id="">『中庭』[中庭編集部]</div></td>
 </tr>
 <tr>
 <th>2013.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/jirenma.php");'><div id="bio_background">「ニッポンのジレンマ」[NHK]/「新ＴＯＫＹＯ論」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("");'><div id="">「ニッポンのジレンマ」[NHK]/「新ＴＯＫＹＯ論」</div></td>
 </tr>
 <tr>
 <th>2013.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/jirenma.php");'><div id="bio_background">『EATER 2014 REBIRTH』[K&Bパブリッシャーズ]</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("");'><div id="">『EATER 2014 REBIRTH』[K&Bパブリッシャーズ]</div></td>
 </tr>
 <tr>
 <th>2013.11</th>
-<td style="cursor: pointer" onclick='dynamic_ex_load("./text/bio_explain/jirenma.php");'><div id="bio_background">「Zigg」/「渋谷の民家「渋家」の6代目代表に齋藤桂太が就任」</div></td>
+<td style="cursor: pointer" onclick='dynamic_ex_load("");'><div id="">「Zigg」/「渋谷の民家「渋家」の6代目代表に齋藤桂太が就任」</div></td>
 </tr>
 <tr>
 <th>2013.10</th>
